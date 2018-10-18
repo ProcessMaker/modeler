98154ef51553696f7642a828dbd1ec31c5b28834
Taylor Dondich <taylor@dondich.com>
2018-10-18 14:53:31 -0700

    Work in progress to bundle components together as a module export.

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+// Our main modeler component
+//import Modeler from '@/components/Modeler';
+
+// Our StatusBar component, if desired
+/*
+import StatusBar from '@/components/statusbar';
+
+// Core BPMN Node controls
+import BaseControls from '@/controls';
+*/
+
+let components = {
+ //   Modeler,
+//    StatusBar,
+ //   BaseControls
+    Hello: () => {
+        throw Error("Test");
+    }
+}
+
+export default components; 
