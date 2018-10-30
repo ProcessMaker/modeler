e0017a059d0b4757f936a2b51ad06a6d181605d0
Taylor Dondich <taylor@dondich.com>
2018-10-29 17:00:24 -0700

    Fix to also export the new node types.

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,24 @@ import Modeler from './Modeler';
 // Our StatusBar component, if desired
 import StatusBar from './statusbar';
 
+import {
+    association,
+    endEvent,
+    exclusiveGateway,
+    sequenceFlow,
+    startEvent,
+    task,
+    textAnnotation
+} from './nodes/index';
+
 export {
     Modeler,
-    StatusBar
-}; 
+    StatusBar,
+    association,
+    endEvent,
+    exclusiveGateway,
+    sequenceFlow,
+    startEvent,
+    task,
+    textAnnotation
+}
