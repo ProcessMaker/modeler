a1fc76c20d808a846f236381a92bc8a859821a94
Vehikl Team <go@vehikl.com>
2019-01-17 16:35:23 -0500

    Only show exclusive gateway

--- src/setup/defaultNodes.js
+++ src/setup/defaultNodes.js
@@ -2,11 +2,37 @@
 import bpmnExtension from '@processmaker/processmaker-bpmn-moddle/resources/processmaker.json';
 
 /* Our initial node types to register with our modeler */
-import * as nodeTypes from '@/components/nodes';
+import {
+  association,
+  endEvent,
+  exclusiveGateway,
+  sequenceFlow,
+  startEvent,
+  task,
+  scriptTask,
+  serviceTask,
+  textAnnotation,
+  pool,
+  poolLane,
+} from '@/components/nodes';
+
+const nodeTypes = [
+  startEvent,
+  endEvent,
+  task,
+  scriptTask,
+  serviceTask,
+  exclusiveGateway,
+  sequenceFlow,
+  textAnnotation,
+  association,
+  pool,
+  poolLane,
+];
 
 window.ProcessMaker.EventBus.$on('modeler-init', ({ registerNode, registerBpmnExtension })  => {
   /* Register basic node types */
-  Object.values(nodeTypes).forEach(config => registerNode(config));
+  nodeTypes.forEach(config => registerNode(config));
 
   /* Add a BPMN extension */
   registerBpmnExtension('pm', bpmnExtension);
