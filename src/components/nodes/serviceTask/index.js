4ea3346915a51f0e5668d52312fbebe6c1a3d792
Robert Komaromi <robertkomaromi@gmail.com>
2019-01-17 14:02:49 -0500

    Add service task. Create fallback for send, receive and user tasks.

---
+++ src/components/nodes/serviceTask/index.js
@@ -0,0 +1,28 @@
+import taskConfig from '@/components/nodes/task';
+
+export default {
+  ...taskConfig,
+  id: 'processmaker-modeler-service-task',
+  bpmnType: ['bpmn:ServiceTask', 'bpmn:SendTask', 'bpmn:ReceiveTask'],
+  control: false,
+  label: 'Service Task',
+  definition(moddle) {
+    return moddle.create('bpmn:ServiceTask', {
+      name: 'New Service Task',
+    });
+  },
+  inspectorConfig: [
+    {
+      name: 'ServiceTask',
+      items: [
+        {
+          component: 'FormText',
+          config: {
+            label: 'Service Task',
+            fontSize: '2em',
+          },
+        },
+      ],
+    },
+  ],
+};
--- src/setup/defaultNodes.js
+++ src/setup/defaultNodes.js
@@ -2,38 +2,12 @@
 import bpmnExtension from '@processmaker/processmaker-bpmn-moddle/resources/processmaker.json';
 
 /* Our initial node types to register with our modeler */
-import {
-  association,
-  endEvent,
-  exclusiveGateway,
-  sequenceFlow,
-  startEvent,
-  task,
-  scriptTask,
-  textAnnotation,
-  pool,
-  poolLane,
-} from '../components/nodes';
+import * as nodeTypes from '@/components/nodes';
 
-const nodeTypes = [
-  startEvent,
-  endEvent,
-  task,
-  scriptTask,
-  exclusiveGateway,
-  sequenceFlow,
-  textAnnotation,
-  association,
-  pool,
-  poolLane,
-];
 window.ProcessMaker.EventBus.$on('modeler-init', ({ registerNode, registerBpmnExtension })  => {
   /* Register basic node types */
-  for (const node of nodeTypes) {
-    registerNode(node);
-  }
+  Object.values(nodeTypes).forEach(config => registerNode(config));
 
   /* Add a BPMN extension */
   registerBpmnExtension('pm', bpmnExtension);
 });
-
