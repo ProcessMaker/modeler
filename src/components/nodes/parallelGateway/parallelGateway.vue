f886b4257297c6304b158b21b557a4369f1f27f1
Tuan Truong <t.truong@vehikl.com>
2019-02-06 15:19:35 -0500

    Add parallel gateway symbol to gateway

--- src/components/nodes/parallelGateway/parallelGateway.vue
+++ src/components/nodes/parallelGateway/parallelGateway.vue
@@ -5,10 +5,12 @@
 
 <script>
 import GatewayComponent from '@/components/nodes/gateway/gateway';
+import parallelGatewaySymbol from '@/assets/parallel-gateway-symbol.svg';
 
 export default {
   extends: GatewayComponent,
   mounted() {
+    this.shape.attr('image/xlink:href', parallelGatewaySymbol);
   },
 };
 </script>
