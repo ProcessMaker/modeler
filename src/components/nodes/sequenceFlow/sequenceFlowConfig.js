230ee2daa7f4e897229e510aac512c915eadeeff
Tuan N. Truong <t.truong@vehikl.com>
2019-02-08 16:54:18 -0500

    Spell Check

--- src/components/nodes/sequenceFlow/sequenceFlowConfig.js
+++ src/components/nodes/sequenceFlow/sequenceFlowConfig.js
@@ -1,5 +1,5 @@
 // Sets the position of the label
 //   - labels are position at the center point of the link,
 //     distance (0 - 1) will cause the label to be positioned relatively to the link length
-//  position label above and eanr the source of the sequence flow
+//  position label above and near the source of the sequence flow
 export const expressionPosition = { distance: 0.25, offset: { x: -40, y: -20 } };
