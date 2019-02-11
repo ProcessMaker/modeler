b7743605ab76d021e2cbbebd9852f47391fa71bf
David Callizaya <davidcallizaya@gmail.com>
2019-02-11 15:54:23 -0400

    Add start timer event component

---
+++ src/components/nodes/startTimerEvent/index.js
@@ -0,0 +1,71 @@
+import component from './startTimerEvent.vue';
+
+export default {
+  id: 'processmaker-modeler-start-timer-event',
+  component,
+  bpmnType: 'bpmn:StartEvent',
+  control: true,
+  category: 'BPMN',
+  icon: require('@/assets/toolpanel/start-timer-event.svg'),
+  label: 'Start Timer Event',
+  definition(moddle) {
+    return moddle.create('bpmn:StartEvent', {
+      name: 'Start Timer Event',
+      eventDefinitions: [
+          moddle.create('bpmn:TimerEventDefinition', {
+              timeDuration: moddle.create('bpmn:Expression', {
+                  body: 'PT1M'
+              })
+          })
+      ],
+    });
+  },
+  diagram(moddle) {
+    return moddle.create('bpmndi:BPMNShape', {
+      bounds: moddle.create('dc:Bounds', {
+        height: 36,
+        width: 36,
+        x: null,
+        y: null,
+      }),
+    });
+  },
+  /**
+   * Validate whether to accept an incoming flow from the node
+   *
+   * @param node
+   */
+  validateIncoming() {
+    return false;
+  },
+  inspectorConfig: [
+    {
+      name: 'Start Timer Event',
+      items: [
+        {
+          component: 'FormText',
+          config: {
+            label: 'Start Timer Event',
+            fontSize: '2em',
+          },
+        },
+        {
+          component: 'FormInput',
+          config: {
+            label: 'Identifier',
+            helper: 'The id field should be unique across all elements in the diagram',
+            name: 'id',
+          },
+        },
+        {
+          component: 'FormInput',
+          config: {
+            label: 'Name',
+            helper: 'The Name of the Start Event',
+            name: 'name',
+          },
+        },
+      ],
+    },
+  ],
+};
---
+++ src/components/nodes/startTimerEvent/startTimerEvent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import joint from 'jointjs';
+import crownConfig from '@/mixins/crownConfig';
+import connectIcon from '@/assets/connect-elements.svg';
+
+export default {
+  props: ['graph', 'node', 'id'],
+  mixins: [crownConfig],
+  data() {
+    return {
+      shape: null,
+      definition: null,
+      crownConfig: [
+        {
+          icon: connectIcon,
+          clickHandler: this.addSequence,
+        },
+      ],
+    };
+  },
+  watch: {
+    'node.definition.name'(name) {
+      this.shape.attr('label/text', name);
+    },
+  },
+  mounted() {
+    // Now, let's add a rounded rect to the graph
+    this.shape = new joint.shapes.standard.Circle();
+    let bounds = this.node.diagram.bounds;
+    this.shape.position(bounds.get('x'), bounds.get('y'));
+    this.shape.resize(bounds.get('width'), bounds.get('height'));
+    this.shape.attr({
+      body: {
+        stroke: '#00bf9c',
+        fill: '#EDFFFC',
+      },
+      label: {
+        text: this.node.definition.get('name'),
+        refY: '130%',
+      },
+    });
+    this.shape.addTo(this.graph);
+    this.shape.component = this;
+  },
+};
+</script>
