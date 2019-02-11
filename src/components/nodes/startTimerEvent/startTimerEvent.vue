05f4797963add7a9a42f7f88278b89cf68a272e5
David Callizaya <davidcallizaya@gmail.com>
2019-02-11 16:55:01 -0400

    Add clock icon for start timer events.

--- src/components/nodes/startTimerEvent/startTimerEvent.vue
+++ src/components/nodes/startTimerEvent/startTimerEvent.vue
@@ -4,48 +4,13 @@
 </template>
 
 <script>
-import joint from 'jointjs';
-import crownConfig from '@/mixins/crownConfig';
-import connectIcon from '@/assets/connect-elements.svg';
+import StartEventComponent from '@/components/nodes/startEvent/startEvent';
+import clockIcon from '@/assets/clock.svg';
 
 export default {
-  props: ['graph', 'node', 'id'],
-  mixins: [crownConfig],
-  data() {
-    return {
-      shape: null,
-      definition: null,
-      crownConfig: [
-        {
-          icon: connectIcon,
-          clickHandler: this.addSequence,
-        },
-      ],
-    };
-  },
-  watch: {
-    'node.definition.name'(name) {
-      this.shape.attr('label/text', name);
-    },
-  },
+  extends: StartEventComponent,
   mounted() {
-    // Now, let's add a rounded rect to the graph
-    this.shape = new joint.shapes.standard.Circle();
-    let bounds = this.node.diagram.bounds;
-    this.shape.position(bounds.get('x'), bounds.get('y'));
-    this.shape.resize(bounds.get('width'), bounds.get('height'));
-    this.shape.attr({
-      body: {
-        stroke: '#00bf9c',
-        fill: '#EDFFFC',
-      },
-      label: {
-        text: this.node.definition.get('name'),
-        refY: '130%',
-      },
-    });
-    this.shape.addTo(this.graph);
-    this.shape.component = this;
+    this.shape.attr('image/xlink:href', clockIcon);
   },
 };
 </script>
