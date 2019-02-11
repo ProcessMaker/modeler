a5c7863284e49be869fb54ce8fd53c937f03398c
Robert Komaromi <robertkomaromi@gmail.com>
2019-02-11 11:34:13 -0500

    Fix stylelint errors

--- src/components/statusbar.vue
+++ src/components/statusbar.vue
@@ -15,18 +15,18 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .statusbar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-top: 1px solid #aaaaaa;
-    background-color: #eeeeee;
-    padding-left: 16px;
-    padding-right: 16px;
-    font-size: 14px;
-    color: #555555;
-    font-weight: normal;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid #aaaaaa;
+  background-color: #eeeeee;
+  padding-left: 16px;
+  padding-right: 16px;
+  font-size: 14px;
+  color: #555555;
+  font-weight: normal;
 }
 </style>
 
