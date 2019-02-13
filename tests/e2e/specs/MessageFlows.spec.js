6a7dbc16db03aa8530330ab06708f65c37688947
Robert Komaromi <robertkomaromi@gmail.com>
2019-02-13 15:56:57 -0500

    Add another message flow test

--- tests/e2e/specs/MessageFlows.spec.js
+++ tests/e2e/specs/MessageFlows.spec.js
@@ -40,4 +40,26 @@ describe('Message Flows', () => {
         expect($links.length).to.eq(numberOfMessageFlowsAdded);
       });
   });
+
+  it('Can connect elements in pools with a message flow', () => {
+    const pool1Position = { x: 250, y: 250 };
+    dragFromSourceToDest('processmaker-modeler-pool', '.paper-container', pool1Position);
+
+    const pool2Position = { x: 250, y: 500 };
+    dragFromSourceToDest('processmaker-modeler-pool', '.paper-container', pool2Position);
+
+    const offset = 100;
+    const taskPosition = { x: pool2Position.x + offset, y: pool2Position.y + offset };
+    dragFromSourceToDest('processmaker-modeler-task', '.paper-container', taskPosition);
+
+    const startEventPosition = { x: 150, y: 150 };
+    connectNodesWithMessageFlow(startEventPosition, taskPosition);
+
+    const numberOfMessageFlowsAdded = 1;
+    getElementAtPosition(taskPosition)
+      .then(getLinksConnectedToElement)
+      .should($links => {
+        expect($links.length).to.eq(numberOfMessageFlowsAdded);
+      });
+  });
 });
