d22535347675d56254d743d4aa46a628996885ab
Tuan Truong <t.truong@vehikl.com>
2019-02-14 11:57:41 -0500

    Replace custom css to use bootstrap util classes

--- src/components/validationStatus.vue
+++ src/components/validationStatus.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <div class="validation-container" v-if="toggleValidationPanel">
-      <span class="validation-container__defaultMessage" v-if="!numberOfValidationErrors">no problems to report</span>
-      <div class="validation-container__list" v-for="error in errorList" :key="error.id">
+    <div class="validation-container position-absolute text-left" v-if="toggleValidationPanel">
+      <span class="validation-container__defaultMessage d-flex justify-content-center align-items-center h-100" v-if="!numberOfValidationErrors">no problems to report</span>
+      <div class="validation-container__list d-flex justify-content-between" v-for="error in errorList" :key="error.id">
         <span class="validation-container__list--id">{{ error.id }}</span>
         <span class="validation-container__list--message">
           <span class="validation-container__list--key">{{ error.errorKey }}</span>
           <div>{{ error.message }}</div>
         </span>
-        <span class="validation-container__list--errorCategory">
+        <span class="validation-container__list--errorCategory d-flex justify-content-center align-items-center">
               <font-awesome-icon class="status-bar-container__status-icon" :style="{ color: iconColor }" :icon="valditionIcon" />
         </span>
       </div>
     </div>
-     <div class="status-bar-container" @click="toggleValidationPanel = !toggleValidationPanel">
+     <div class="status-bar-container d-flex align-items-center justify-content-around" @click="toggleValidationPanel = !toggleValidationPanel">
       <span class="statusBar-container__status-text">Problems {{ numberOfValidationErrors }}</span>
       <font-awesome-icon class="status-bar-container__status-icon" :style="{ color: statusColor }" :icon="statusIcon" />
       <font-awesome-icon class="status-bar-container__status-ellipsis" :icon="ellipsisIcon" />
@@ -104,19 +104,15 @@ $border-color: #aaaaaa;
 $text-size-sm: 0.85rem;
 $id-container-width: 6rem;
 $message-container-width: 18rem;
+$error-category-width: 1rem;
 $validation-container-height: 20rem;
 $validation-container-width: 28rem;
 $status-bar-container-height: 2.5rem;
 $status-bar-container-width: 8rem;
-$message-label-pill-width: 1.5rem;
-$message-label-pill-height: 4rem;
 $error-color: #D9534F;
 $warning-color: #F0AD4E;
 
 .status-bar-container {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
   font-size: $text-size-sm;
   color: $seconadry-grey;
   height: $status-bar-container-height;
@@ -124,6 +120,7 @@ $warning-color: #F0AD4E;
   cursor: pointer;
 
   &__status-ellipsis {
+
     &:hover {
       color: $secondary-blue;
     }
@@ -131,51 +128,45 @@ $warning-color: #F0AD4E;
 }
 
 .validation-container {
-  position:absolute;
-  bottom:0;
-  right:0;
+  bottom: 0;
+  right: 0;
   height: $validation-container-height;
   width: $validation-container-width;
   background-color: $primary-white;
   overflow: scroll;
   margin-bottom: 2.5rem;
   border: 1px solid $border-color;
-  text-align: left;
 
   &__defaultMessage {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
     text-transform: capitalize;
   }
 
   &__list {
-    display: grid;
-    grid-template-columns: $id-container-width $message-container-width 1fr;
     padding: 1rem;
     word-wrap: break-word;
 
     &--id {
+      width: $id-container-width;
       color: $seconadry-grey;
       text-transform: none;
       margin-right: 1rem;
     }
 
     &--message {
+      width: $message-container-width;
+      margin-right: 1rem;
       text-transform: capitalize;
     }
 
     &--errorCategory {
-      justify-self: center;
-      align-self: center;
+      width: $error-category-width;
     }
 
     &--key {
       font-weight: 700;
     }
-
   }
+
   .label-background-warning {
     background-color: $warning-color;
   }
