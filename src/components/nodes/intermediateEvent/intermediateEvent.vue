e007c8d4be7d25365368c161f2165e148fca9dc4
David Callizaya <davidcallizaya@gmail.com>
2019-02-14 09:38:38 -0400

    Add intermediate event

---
+++ src/components/nodes/intermediateEvent/intermediateEvent.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import crownConfig from '@/mixins/crownConfig';
+import connectIcon from '@/assets/connect-elements.svg';
+import EventShape from '@/components/nodes/intermediateEvent/shape';
+
+export default {
+  props: ['graph', 'node', 'id'],
+  mixins: [crownConfig],
+  data() {
+    return {
+      shape: null,
+      definition: null,
+      crownConfig: [
+        {
+          icon: connectIcon,
+          clickHandler: this.addSequence,
+        },
+      ],
+    };
+  },
+  watch: {
+    'node.definition.name'(name) {
+      this.shape.attr('label/text', name);
+    },
+  },
+  mounted() {
+    // Now, let's add a rounded rect to the graph
+    this.shape = new EventShape();
+    let bounds = this.node.diagram.bounds;
+    this.shape.position(bounds.get('x'), bounds.get('y'));
+    this.shape.resize(bounds.get('width'), bounds.get('height'));
+    this.shape.attr({
+      body: {
+        stroke: '#FBBE02',
+        fill: '#FFF4D1',
+      },
+      body2: {
+        stroke: '#FBBE02',
+        fill: '#FFF4D1',
+      },
+      label: {
+        text: this.node.definition.get('name'),
+        refY: '130%',
+      },
+      image: {
+        'ref-x': 5,
+        'ref-y': 5,
+        'width': bounds.get('width') - 10,
+        'height': bounds.get('height') - 10,
+      },
+    });
+    this.shape.addTo(this.graph);
+    this.shape.component = this;
+  },
+};
+</script>
