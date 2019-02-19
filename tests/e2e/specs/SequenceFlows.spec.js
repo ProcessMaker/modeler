94e348d56f4a92e98aa3873674fe4068c5c26d5a
Tuan N. Truong <t.truong@vehikl.com>
2019-02-19 11:18:14 -0500

    Create SequenceFlows.spec

---
+++ tests/e2e/specs/SequenceFlows.spec.js
@@ -0,0 +1,12 @@
+import {
+  dragFromSourceToDest,
+  getElementAtPosition,
+  getCrownButtonForElement,
+  getLinksConnectedToElement,
+} from '../support/utils';
+
+describe('Sequence Flows', () => {
+  beforeEach(() => {
+    cy.loadModeler();
+  });
+});
