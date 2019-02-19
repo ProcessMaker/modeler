89c942982c9a86e16b35fa8917993467b4c53910
Tuan N. Truong <t.truong@vehikl.com>
2019-02-19 13:40:29 -0500

    Add test to check sequence flow has the highest z-index

--- tests/e2e/specs/SequenceFlows.spec.js
+++ tests/e2e/specs/SequenceFlows.spec.js
@@ -26,4 +26,24 @@ describe('Sequence Flows', () => {
         expect($links.length).to.eq(numberOfSequenceFlowsAdded);
       });
   });
+
+  it('Have the highest z-index', () => {
+    const startEventPosition = { x: 150, y: 150 };
+    const taskPosition = { x: 250, y: 250 };
+    const poolPosition = { x: 150, y: 150 };
+
+    dragFromSourceToDest('processmaker-modeler-task', '.paper-container', taskPosition);
+
+    connectNodesWithFlow('sequence-flow-button', startEventPosition, taskPosition);
+
+    dragFromSourceToDest('processmaker-modeler-pool', '.paper-container', poolPosition);
+
+    // getLastCell - will get the highest cell (element or link) with the highest z property
+    cy.window().its('store.state.graph')
+      .invoke('getLastCell').
+      then(cell => {
+        const sequenceFlow = 'processmaker-modeler-sequence-flow';
+        expect(cell.component.node.type).to.eq(sequenceFlow);
+      });
+  });
 });
