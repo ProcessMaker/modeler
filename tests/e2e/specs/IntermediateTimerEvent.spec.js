703656cc6273120d60260722af379690241568b9
Vehikl Team <go@vehikl.com>
2019-02-20 16:20:07 -0500

    Add test spec

---
+++ tests/e2e/specs/IntermediateTimerEvent.spec.js
@@ -0,0 +1,12 @@
+import {
+  dragFromSourceToDest,
+  getElementAtPosition,
+  getLinksConnectedToElement,
+  connectNodesWithFlow,
+} from '../support/utils';
+
+describe('Intermediate Timer Event', () => {
+  beforeEach(() => {
+    cy.loadModeler();
+  });
+});
