6326ad9e03dddf04c3d246f0f56e61977fef78f3
Vehikl Team <go@vehikl.com>
2019-02-21 09:38:29 -0500

    Add test to update name

--- tests/e2e/specs/IntermediateTimerEvent.spec.js
+++ tests/e2e/specs/IntermediateTimerEvent.spec.js
@@ -1,6 +1,8 @@
 import {
   dragFromSourceToDest,
   getElementAtPosition,
+  typeIntoTextInput,
+  waitToRenderAllShapes,
   getLinksConnectedToElement,
   connectNodesWithFlow,
 } from '../support/utils';
@@ -8,5 +10,29 @@ import {
 describe('Intermediate Timer Event', () => {
   beforeEach(() => {
     cy.loadModeler();
+  });
+
+  it.only('Update task name', () => {
+    const testString = 'testing';
+
+    dragFromSourceToDest(
+      'processmaker-modeler-intermediate-catch-timer-event',
+      '.paper-container',
+      { x: 200, y: 200 }
+    );
+
+    waitToRenderAllShapes();
+
+    getElementAtPosition({ x: 200, y: 200})
+      .click();
+
+    typeIntoTextInput('[name=\'name\']', testString);
+
+    typeIntoTextInput('[name=\'name\']', testString);
+    typeIntoTextInput('[name=\'name\']', testString);
+
+    // cy.get('.joint-viewport').find('.joint-type-processmaker-components-nodes-task').click({ force: true });
+
+    cy.get('[name=\'name\']').should('have.value', testString);
   });
 });
