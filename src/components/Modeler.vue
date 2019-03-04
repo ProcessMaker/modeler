95acbd26b71a23efd735642d6ea59e208529b8e2
Vehikl Team <go@vehikl.com>
2019-03-04 16:14:33 -0500

    Remove debounce from Modeler

--- src/components/Modeler.vue
+++ src/components/Modeler.vue
@@ -65,7 +65,6 @@ import controls from './controls';
 import { highlightPadding } from '@/mixins/crownConfig';
 import uniqueId from 'lodash/uniqueId';
 import pull from 'lodash/pull';
-import debounce from 'lodash/debounce';
 import { startEvent } from '@/components/nodes';
 import store from '@/store';
 import InspectorPanel from '@/components/inspectors/InspectorPanel';
@@ -585,8 +584,6 @@ export default {
     },
   },
   created() {
-    this.loadXML = debounce(this.loadXML.bind(this), 0, { leading: false });
-
     /* Initialize the BpmnModdle and its extensions */
     window.ProcessMaker.EventBus.$emit('modeler-init', {
       registerInspectorExtension: this.registerInspectorExtension,
