ca7d9ff70f3e281eda353a5de4dc654f22e158d8
Tuan N. Truong <t.truong@vehikl.com>
2019-03-05 10:53:15 -0500

    Create Event Based Gateway node

---
+++ src/components/nodes/eventBasedGateway/eventBasedGateway.vue
@@ -0,0 +1,15 @@
+<template>
+  <div/>
+</template>
+
+<script>
+import GatewayComponent from '@/components/nodes/gateway/gateway';
+
+
+export default {
+  extends: GatewayComponent,
+  mounted() {
+
+  },
+};
+</script>
