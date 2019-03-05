eb46b629dad4d6c1d9c48b34e365b67645f271e2
Tuan N. Truong <t.truong@vehikl.com>
2019-03-05 13:41:20 -0500

    Add gateway symbol

--- src/components/nodes/eventBasedGateway/eventBasedGateway.vue
+++ src/components/nodes/eventBasedGateway/eventBasedGateway.vue
@@ -4,12 +4,13 @@
 
 <script>
 import GatewayComponent from '@/components/nodes/gateway/gateway';
+import eventBasedSymbol from '@/assets/event-based-gateway-symbol.svg';
 
 
 export default {
   extends: GatewayComponent,
   mounted() {
-
+    this.shape.attr('image/xlink:href', eventBasedSymbol);
   },
 };
 </script>
