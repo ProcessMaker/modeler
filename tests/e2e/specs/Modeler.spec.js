002c4b7c67ebfd616e35d12be442126cc37f3a78
Tuan N. Truong <t.truong@vehikl.com>
2019-03-13 11:47:05 -0400

    Fix failing testing

--- tests/e2e/specs/Modeler.spec.js
+++ tests/e2e/specs/Modeler.spec.js
@@ -179,7 +179,7 @@ describe('Modeler', () => {
 
     getElementAtPosition(gatewayPosition).click();
 
-    cy.get('[name=gatewayDirection]').select('converging');
+    cy.get('[name=gatewayDirection]').select('Converging');
 
     waitToRenderNodeUpdates();
 
