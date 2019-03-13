e64fe4efa5839a6d6092a3d194cca37a709f4e35
Tuan N. Truong <t.truong@vehikl.com>
2019-03-13 15:37:22 -0400

    Update XML on test

--- tests/e2e/specs/StartTimerEvent.spec.js
+++ tests/e2e/specs/StartTimerEvent.spec.js
@@ -16,19 +16,19 @@ describe('Start Timer Event', () => {
     const validStartEventTimerXML = `<?xml version="1.0" encoding="UTF-8"?>
     <bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" id="Definitions_03dabax" targetNamespace="http://bpmn.io/schema/bpmn" exporter="ProcessMaker Modeler" exporterVersion="1.0">
       <bpmn:process id="Process_1" isExecutable="true">
-        <bpmn:startEvent id="node_2" name="Start Event" />
-        <bpmn:startEvent id="node_4" name="testing">
+        <bpmn:startEvent id="node_1" name="Start Event" />
+        <bpmn:startEvent id="node_2" name="testing">
           <bpmn:timerEventDefinition>
-            <bpmn:timeCycle>R/2019-02-25T00:30-05:00/P1W</bpmn:timeCycle>
+            <bpmn:timeCycle>2019-03-13T00:00-04:00|R/2019-03-18T00:00-04:00/P21W|R/2019-03-13T00:00-04:00/P21W</bpmn:timeCycle>
           </bpmn:timerEventDefinition>
         </bpmn:startEvent>
       </bpmn:process>
       <bpmndi:BPMNDiagram id="BPMNDiagram_1">
         <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
-          <bpmndi:BPMNShape id="node_2_di" bpmnElement="node_2">
+          <bpmndi:BPMNShape id="node_1_di" bpmnElement="node_1">
             <dc:Bounds x="150" y="150" width="36" height="36" />
           </bpmndi:BPMNShape>
-          <bpmndi:BPMNShape id="node_4_di" bpmnElement="node_4">
+          <bpmndi:BPMNShape id="node_2_di" bpmnElement="node_2">
             <dc:Bounds x="250.76040649414062" y="250.00347137451172" width="36" height="36" />
           </bpmndi:BPMNShape>
         </bpmndi:BPMNPlane>
