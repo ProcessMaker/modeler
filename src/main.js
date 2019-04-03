deca0a0735ea190dd7f80e490ab407571b9bc9f7
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-04-03 15:15:25 -0400

    Add library i18next

--- src/main.js
+++ src/main.js
@@ -12,8 +12,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import i18next from 'i18next';
+import VueI18Next from '@panter/vue-i18next';
 Vue.config.productionTip = false;
+
+Vue.use(VueI18Next);
+i18next.init({lng: 'en'});
+Vue.mixin({ i18n: new VueI18Next(i18next) });
 
 new Vue({
   render: h => h(ModelerApp),
