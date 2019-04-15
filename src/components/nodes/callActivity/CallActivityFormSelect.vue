acbb8fc0d0a8fe58bfeca8b865ff60bec166dc33
Robert Komaromi <robertkomaromi@gmail.com>
2019-04-15 15:59:47 -0400

    Remove unneeded commented code

--- src/components/nodes/callActivity/CallActivityFormSelect.vue
+++ src/components/nodes/callActivity/CallActivityFormSelect.vue
@@ -25,15 +25,6 @@ export default {
         uniqBy(process.events, 'ownerProcessId').forEach(event => {
           list.push(this.toDropdownFormat(process, event));
         });
-
-        // const subprocessList = [];
-
-        // process.events.forEach(event => {
-        //   if (!subprocessList.includes(event.ownerProcessId)) {
-        //     subprocessList.push(event.ownerProcessId);
-        //     list.push(this.toDropdownFormat(process, event));
-        //   }
-        // });
       });
 
       return list;
@@ -42,13 +33,6 @@ export default {
   methods: {
     containsMultipleProcesses(process) {
       return uniqBy(process.events, 'ownerProcessId').length > 1;
-      // const subprocessList = [];
-      // process.events.forEach((event) => {
-      //   if (!subprocessList.includes(event.ownerProcessId)) {
-      //     subprocessList.push(event.ownerProcessId);
-      //   }
-      // });
-      // return subprocessList.length > 1;
     },
     toDropdownFormat(process, event) {
       return {
