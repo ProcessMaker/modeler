18e680bc6d72feb7e56d4109aba39f5a28c0c6c6
Tuan N. Truong <t.truong@vehikl.com>
2019-05-16 10:03:23 -0400

    Move gateway direction in accordion

--- src/components/nodes/inclusiveGateway/index.js
+++ src/components/nodes/inclusiveGateway/index.js
@@ -27,12 +27,6 @@ export default {
     {
       name: 'Inclusive Gateway',
       items: [
-        // {
-        //   component: 'FormText',
-        //   config: {
-        //     label: 'Inclusive Gateway',
-        //   },
-        // },
         {
           component: 'FormAccordion',
           container: true,
@@ -59,19 +53,19 @@ export default {
                 name: 'name',
               },
             },
+            {
+              component: 'FormSelect',
+              config: {
+                label: 'Direction',
+                helper: 'Select direction of gateway',
+                name: 'gatewayDirection',
+                options: [
+                  { value: gatewayDirection.diverging , content: 'Diverging' },
+                  { value: gatewayDirection.converging , content: 'Converging' },
+                ],
+              },
+            },
           ],
-        },
-        {
-          component: 'FormSelect',
-          config: {
-            label: 'Direction',
-            helper: 'Select direction of gateway',
-            name: 'gatewayDirection',
-            options: [
-              { value: gatewayDirection.diverging , content: 'Diverging' },
-              { value: gatewayDirection.converging , content: 'Converging' },
-            ],
-          },
         },
       ],
     },
