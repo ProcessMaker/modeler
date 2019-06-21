42b6db8f47788d915b3cdd00f343400f310a7415
Tuan N. Truong <t.truong@vehikl.com>
2019-06-21 10:30:51 -0400

    Add test

--- tests/e2e/specs/TextAnnotation.spec.js
+++ tests/e2e/specs/TextAnnotation.spec.js
@@ -2,6 +2,8 @@ import {
   dragFromSourceToDest,
   typeIntoTextInput,
   getElementAtPosition,
+  connectNodesWithFlow,
+  getCrownButtonForElement,
 } from '../support/utils';
 
 import { nodeTypes } from '../support/constants';
@@ -21,5 +23,45 @@ describe('Text Annotation', () => {
 
     typeIntoTextInput('[name=text]', testString);
     cy.get('[name=text]').should('have.value', testString);
+  });
+
+  it('save a process with text annotation, pool and lane ', () => {
+    if (Cypress.env('inProcessmaker')) {
+      this.skip();
+    }
+
+    const poolPosition = { x: 250, y: 250 };
+    dragFromSourceToDest(nodeTypes.pool, poolPosition);
+
+    getElementAtPosition(poolPosition)
+      .click()
+      .then($pool => {
+        getCrownButtonForElement($pool, 'lane-above-button').click({ force: true });
+        getCrownButtonForElement($pool, 'lane-below-button').click({ force: true });
+      });
+
+
+    const textAnnotationPosition = { x: 400, y: 100 };
+    dragFromSourceToDest(nodeTypes.textAnnotation, textAnnotationPosition);
+
+    const taskPosition = { x: 400, y: 300 };
+    dragFromSourceToDest(nodeTypes.task, taskPosition);
+
+    connectNodesWithFlow('association-flow-button', textAnnotationPosition, taskPosition);
+
+
+    cy.get('[data-test=save-process]').click();
+
+    const successMessage = 'The process was saved.';
+
+    cy.get('.alert-success').contains(successMessage);
+
+    getElementAtPosition(poolPosition)
+      .click()
+      .then($pool => {
+        getCrownButtonForElement($pool, 'delete-button').click({ force: true });
+      });
+
+    cy.get('[data-test=save-process]').click();
   });
 });
