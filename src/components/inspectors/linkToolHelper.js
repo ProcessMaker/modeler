29d89a6089b6151a612264d80a35b6bd75850de3
Tuan N. Truong <t.truong@vehikl.com>
2019-07-12 16:37:55 -0400

    Change copy of helper

--- src/components/inspectors/linkToolHelper.js
+++ src/components/inspectors/linkToolHelper.js
@@ -4,7 +4,7 @@ const linkToolHelper = {
       <span class="link-tool-helper__title">Link Tool Helper</span>
       <div class="link-tool-helper__body">
         <div class="link-tool-helper__body--text">Double click vertices to delete</div>
-        <div class="link-tool-helper__body--text">Anchor points can be changed by dragging</div>
+        <div class="link-tool-helper__body--text">Anchor points can be set by dragging flow to desired point</div>
       </div>
     </div>
   `,
