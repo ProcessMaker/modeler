0eaaec06f410dd73422e126ae0f406752da0e446
Tuan N. Truong <t.truong@vehikl.com>
2019-08-01 09:41:25 -0400

    Add attributes to boundary timer event

--- src/components/nodes/boundaryTimerEvent/index.js
+++ src/components/nodes/boundaryTimerEvent/index.js
@@ -1,5 +1,7 @@
 import component from './boundaryTimerEvent.vue';
 import idConfigSettings from '@/components/inspectors/idConfigSettings';
+import IntermediateTimer from '../../inspectors/IntermediateTimer.vue';
+export const defaultDurationValue = 'PT1H';
 
 export default {
   id: 'processmaker-modeler-boundary-timer-event',
@@ -12,10 +14,13 @@ export default {
   definition(moddle, $t) {
     return moddle.create('bpmn:BoundaryEvent', {
       name: $t('New Boundary Timer Event'),
+      nonInterrupting: false,
       attachedToRef: '',
       eventDefinitions: [
         moddle.create('bpmn:TimerEventDefinition', {
-          id: null,
+          timeDuration: moddle.create('bpmn:Expression', {
+            body: defaultDurationValue,
+          }),
         }),
       ],
     });
@@ -27,6 +32,49 @@ export default {
         width: 36,
       }),
     });
+  },
+  inspectorData(node) {
+    return Object.entries(node.definition).reduce((data, [key, value]) => {
+      if (key === 'eventDefinitions') {
+        const type = Object.keys(value[0])[1];
+        const body = value[0][type].body;
+        data[key] = { type, body };
+      } else {
+        data[key] = value;
+      }
+
+      return data;
+    }, {});
+  },
+  inspectorHandler(value, node, setNodeProp, moddle) {
+    const definition = node.definition;
+
+    // Go through each property and rebind it to our data
+    for (const key in value) {
+      if (definition[key] === value[key]) {
+        continue;
+      }
+
+      if (key === 'eventDefinitions') {
+        const { type, body } = value[key];
+
+        const expression = definition.get(key)[0][type];
+        if (expression && expression.body === body) {
+          continue;
+        }
+
+        const eventDefinition = {
+          [type]: moddle.create('bpmn:Expression', { body }),
+        };
+
+        const eventDefinitions = [
+          moddle.create('bpmn:TimerEventDefinition', eventDefinition),
+        ];
+        setNodeProp(node, 'eventDefinitions', eventDefinitions);
+      } else {
+        setNodeProp(node, key, value[key]);
+      }
+    }
   },
   inspectorConfig: [
     {
@@ -52,6 +100,33 @@ export default {
                 label: 'Name',
                 helper: 'The Name of the Boundary Timer Event',
                 name: 'name',
+              },
+            },
+            {
+              component: 'FormCheckbox',
+              config: {
+                label: 'Non-interrupting',
+                name: 'nonInterrupting',
+                helper: 'Boundary Event Type',
+              },
+            },
+          ],
+        },
+        {
+          component: 'FormAccordion',
+          container: true,
+          config: {
+            label: 'Timing Control',
+            icon: 'clock',
+            name: 'timing-control',
+          },
+          items: [
+            {
+              component: IntermediateTimer,
+              config: {
+                label: 'Name',
+                helper: 'Time expression',
+                name: 'eventDefinitions',
               },
             },
           ],
