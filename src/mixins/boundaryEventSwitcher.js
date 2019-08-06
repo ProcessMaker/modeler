af6343739deeb021e307bb83408cdf0cfc66988d
Tuan N. Truong <t.truong@vehikl.com>
2019-08-06 13:36:04 -0400

    Prevent boundary events toggling to intermediate timer event

--- src/mixins/boundaryEventSwitcher.js
+++ src/mixins/boundaryEventSwitcher.js
@@ -68,6 +68,7 @@ export default {
 
       if (
         (selfIsBoundaryEvent && task) ||
+        (selfIsBoundaryEvent && !task) ||
         (!selfIsBoundaryEvent && !task)
       ) {
         return;
