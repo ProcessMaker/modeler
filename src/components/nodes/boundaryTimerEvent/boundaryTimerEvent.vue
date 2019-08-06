d2c78ba582604b4e79581b29acc430ee74dfc6d1
Tuan N. Truong <t.truong@vehikl.com>
2019-08-06 14:41:11 -0400

    Snap to ports

--- src/components/nodes/boundaryTimerEvent/boundaryTimerEvent.vue
+++ src/components/nodes/boundaryTimerEvent/boundaryTimerEvent.vue
@@ -6,7 +6,26 @@
 import BoundaryEvent from '@/components/nodes/boundaryEvent/boundaryEvent';
 import timerEventIcon from '@/assets/timer-event-icon.svg';
 import boundaryEventSwitcher from '@/mixins/boundaryEventSwitcher';
+import { portGroups } from '@/mixins/portsConfig';
+import joint from 'jointjs';
 
+function getPointFromGroup(view, group) {
+  const { x: shapeX, y: shapeY } = view.position();
+  const { x, y } = Object.values(view.getPortsPositions(group))[0];
+
+  return new joint.g.Point(shapeX + x, shapeY + y);
+}
+
+function getPortPoints(view) {
+  return portGroups.map(group => getPointFromGroup(view, group));
+}
+
+function closestPort(endView, anchorReference) {
+  return getPortPoints(endView).sort((p1, p2) => {
+    const referencePoint = new joint.g.Point(anchorReference.x, anchorReference.y);
+    return referencePoint.distance(p1) - referencePoint.distance(p2);
+  })[0];
+}
 export default {
   extends: BoundaryEvent,
   mixins: [boundaryEventSwitcher],
@@ -27,6 +46,9 @@ export default {
 
     if (task) {
       task.embed(this.shape);
+      const { x, y } = closestPort(task, this.shape.position());
+      const { width } = this.shape.size();
+      this.shape.position(x - (width / 2), y - (width / 2));
     } else {
       this.$emit('remove-node', this.node);
     }
