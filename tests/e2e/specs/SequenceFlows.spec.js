0c4da02992f03dd3bc7f4a5b4c39d5a0f418b093
Robert Komaromi <robertkomaromi@gmail.com>
2019-08-15 14:06:07 -0400

    Add tests for sequence flow anchor point saving

--- tests/e2e/specs/SequenceFlows.spec.js
+++ tests/e2e/specs/SequenceFlows.spec.js
@@ -4,6 +4,8 @@ import {
   getElementAtPosition,
   getLinksConnectedToElement,
   typeIntoTextInput,
+  moveElement,
+  waitToRenderAllShapes,
 } from '../support/utils';
 import { nodeTypes } from '../support/constants';
 
@@ -121,5 +123,99 @@ describe('Sequence Flows', () => {
 
     checkAnchorPoints('source');
     checkAnchorPoints('target');
+  });
+
+  it('Retains target anchor point after parsing and moving shape', function() {
+    const startEventPosition = { x: 150, y: 150 };
+    const taskPosition = { x: 200, y: 300 };
+    dragFromSourceToDest(nodeTypes.task, taskPosition);
+
+    connectNodesWithFlow('sequence-flow-button', startEventPosition, taskPosition);
+
+    getElementAtPosition(taskPosition)
+      .then(getLinksConnectedToElement)
+      .then($links => $links[0])
+      .click('topRight');
+
+    waitToRenderAllShapes();
+
+    const anchorSelector = '[data-tool-name=target-anchor]';
+
+    cy.get(anchorSelector).then(() => {
+      /* Move anchor to right */
+      cy.get(anchorSelector).trigger('mousedown');
+      cy.get(anchorSelector).trigger('mousemove', 200, 1, { force: true });
+      cy.get(anchorSelector).trigger('mouseup', { force: true });
+    });
+
+    moveElement(taskPosition, taskPosition.y + 50, taskPosition.y);
+
+    cy.get('[data-test=undo]').click();
+
+    const newTaskPosition = { x: taskPosition.x + 300, y: taskPosition.y };
+    moveElement(taskPosition, newTaskPosition.x, newTaskPosition.y);
+
+    getElementAtPosition(newTaskPosition)
+      .then(getLinksConnectedToElement)
+      .then($links => $links[0])
+      .click('topRight');
+
+    cy.get(anchorSelector).then($anchor => {
+      /* Anchor should still be on the right */
+      const { left } = $anchor.position();
+
+      getElementAtPosition(newTaskPosition).then($task => {
+        const { left: taskleft } = $task.position();
+
+        expect(left).to.be.greaterThan(taskleft);
+      });
+    });
+  });
+
+  it('Retains source anchor point after parsing and moving shape', function() {
+    const startEventPosition = { x: 150, y: 150 };
+    const taskPosition = { x: 400, y: 400 };
+    dragFromSourceToDest(nodeTypes.task, taskPosition);
+
+    connectNodesWithFlow('sequence-flow-button', startEventPosition, taskPosition);
+
+    getElementAtPosition(startEventPosition)
+      .then(getLinksConnectedToElement)
+      .then($links => $links[0])
+      .click('topRight');
+
+    waitToRenderAllShapes();
+
+    const anchorSelector = '[data-tool-name=source-anchor]';
+
+    cy.get(anchorSelector).then(() => {
+      /* Move anchor to bottom */
+      cy.get(anchorSelector).trigger('mousedown');
+      cy.get(anchorSelector).trigger('mousemove', 1, 200, { force: true });
+      cy.get(anchorSelector).trigger('mouseup', { force: true });
+    });
+
+    moveElement(startEventPosition, startEventPosition.y + 50, startEventPosition.y);
+
+    cy.get('[data-test=undo]').click();
+
+    const newStartEventPosition = { x: startEventPosition.x, y: startEventPosition.y + 200 };
+    moveElement(startEventPosition, newStartEventPosition.x, newStartEventPosition.y);
+
+    getElementAtPosition(newStartEventPosition)
+      .then(getLinksConnectedToElement)
+      .then($links => $links[0])
+      .click('topRight');
+
+    cy.get(anchorSelector).then($anchor => {
+      /* Anchor should still be on the bottom */
+      const { top } = $anchor.position();
+
+      getElementAtPosition(newStartEventPosition).then($task => {
+        const { top: startEventTop } = $task.position();
+
+        expect(top).to.be.greaterThan(startEventTop);
+      });
+    });
   });
 });
