41eee6d84aa62c60865a071adb2e40e5ac3c93b1
Nolan Ehrstrom <github@nolanpro.com>
2019-08-19 17:26:01 -0700

    Refactor registerInspectorExtension and add test placeholder

--- src/components/Modeler.vue
+++ src/components/Modeler.vue
@@ -333,24 +333,43 @@ export default {
      * component is already registered, it is replaced by the new one.
      */
     registerInspectorExtension(node, config) {
-      const registeredIndex = node.inspectorConfig[0].items.findIndex(item => {
+      this.addToInspector(
+        this.inspectorItemsToAddTo(node, config),
+        this.existingConfigIndex(node, config),
+        config
+      );
+    },
+    addToInspector(inspectorItems, existingIndex, config) {
+      if (existingIndex === -1) {
+        inspectorItems.push(config);
+      } else {
+        inspectorItems[existingIndex] = config;
+      }
+    },
+    existingConfigIndex(node, config) {
+      return this.inspectorItems(node).findIndex(item => {
         return config.id && config.id === item.id;
       });
-        
-      let items;
-      if (config.container) {
-        // if it is a container, don't put it inside the default container
-        items = node.inspectorConfig[0].items;
+    },
+    isContainer(config)
+    {
+      return typeof config.container !== 'undefined';
+    },
+    inspectorItemsToAddTo(node, config) {
+      if (this.isContainer(config)) {
+        return this.inspectorItems(node);
       } else {
-        // if it is not a container, put it in the default container
-        items = node.inspectorConfig[0].items[0].items;
+        // if the config we're adding is not a container, put it in the default container
+        return this.defaultContainerItems(node);
       }
-
-      if (registeredIndex === -1) {
-        items.push(config);
-      } else {
-        items[registeredIndex] = config;
-      }
+    },
+    defaultContainerItems(node)
+    {
+      return this.inspectorItems(node)[0].items;
+    },
+    inspectorItems(node)
+    {
+      return node.inspectorConfig[0].items;
     },
     /**
      * Register a BPMN Moddle extension in order to support extensions to the bpmn xml format.
---
+++ tests/unit/Modeler.spec.js
@@ -0,0 +1,8 @@
+import Modeler from '../../src/components/Modeler.vue';
+
+describe('registerInspectorExtension', () => {
+    it('adds config with a container', () => {
+        // Placeholder
+        expect(typeof Modeler.created).toBe('function')
+    })
+})
