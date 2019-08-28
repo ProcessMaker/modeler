f76c5bdfbb50f0ed186594d59e6e03ac1e800252
Ryan Cooley <ryan.cooley@processmaker.com>
2019-08-28 14:58:49 -0700

    Add Advanced accordion to pool node

--- src/components/nodes/pool/index.js
+++ src/components/nodes/pool/index.js
@@ -41,14 +41,26 @@ export default {
           items: [
             {
               component: 'FormInput',
-              config: idConfigSettings,
-            },
-            {
-              component: 'FormInput',
               config: {
                 ...nameConfigSettings,
                 helper: 'The Name of the Pool',
               },
+            },
+          ],
+        },
+        {
+          component: 'FormAccordion',
+          container: true,
+          config: {
+            initiallyOpen: false,
+            label: 'Advanced',
+            icon: 'cogs',
+            name: 'advanced',
+          },
+          items: [
+            {
+              component: 'FormInput',
+              config: idConfigSettings,
             },
           ],
         },
