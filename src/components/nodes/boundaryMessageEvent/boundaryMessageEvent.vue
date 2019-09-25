3b9253e2ebdf7c27da157b2bfb4edd2cb9ad0da6
sanjacornelius <sanja.cornelius@processmaker.com>
2019-09-25 08:50:05 -0700

    create seperate mixin file for duplicate icon

--- src/components/nodes/boundaryMessageEvent/boundaryMessageEvent.vue
+++ src/components/nodes/boundaryMessageEvent/boundaryMessageEvent.vue
@@ -1,29 +1,9 @@
 <script>
 import BoundaryEvent from '@/components/nodes/boundaryEvent/boundaryEvent';
-import intermediateMailSymbol from '@/assets/intermediate-mail.svg';
+import intermediateMailIcon from '../../../mixins/intermediateMailIcon';
 
 export default {
   extends: BoundaryEvent,
-  mounted() {
-    this.shape.attr('image/xlink:href', intermediateMailSymbol);
-    this.shape.attr({
-      body: {
-        stroke: '#000',
-        strokeWidth: 1,
-        fill: '#fff',
-      },
-      body2: {
-        stroke: '#000',
-        strokeWidth: 1,
-        fill: '#fff',
-      },
-      image: {
-        width: 20,
-        height: 20,
-        y: 3,
-        x: 3,
-      },
-    });
-  },
+  mixins: [intermediateMailIcon],
 };
 </script>
