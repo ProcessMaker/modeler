523b8ffe876246836194359f2c5b8bb98a9e78e1
Jeremy Cook <jeremycook0@icloud.com>
2019-09-25 13:30:25 -0400

    Small refactoring cleanup.

--- tests/e2e/specs/AssociationFlows.spec.js
+++ tests/e2e/specs/AssociationFlows.spec.js
@@ -11,12 +11,6 @@ import { nodeTypes } from '../support/constants';
 describe('Association Flows', () => {
   it('Change direction of association to none, one and both', () => {
     const directionSelectSelector = '[name=associationDirection]';
-    const testDirection = {
-      none: `${direction.none}`,
-      one: `${direction.one}`,
-      both: `${direction.both}`,
-    };
-
     const textAnnotationPosition = { x: 400, y: 100 };
     dragFromSourceToDest(nodeTypes.textAnnotation, textAnnotationPosition);
 
@@ -31,12 +25,12 @@ describe('Association Flows', () => {
       .click();
 
     cy.get(directionSelectSelector).select('None');
-    cy.get(directionSelectSelector).should('have.value', testDirection.none);
+    cy.get(directionSelectSelector).should('have.value', direction.none);
 
     cy.get(directionSelectSelector).select('One');
-    cy.get(directionSelectSelector).should('have.value', testDirection.one);
+    cy.get(directionSelectSelector).should('have.value', direction.one);
 
     cy.get(directionSelectSelector).select('Both');
-    cy.get(directionSelectSelector).should('have.value', testDirection.both);
+    cy.get(directionSelectSelector).should('have.value', direction.both);
   });
 });
