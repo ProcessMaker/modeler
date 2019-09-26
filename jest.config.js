3af74cbb71c224fa51053456d1ea62069da3cd15
Jeremy Cook <jeremycook0@icloud.com>
2019-09-26 10:12:57 -0400

    Cleanup Jest config.

--- jest.config.js
+++ jest.config.js
@@ -20,9 +20,8 @@ module.exports = {
     'jest-serializer-vue',
   ],
   testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)',
   ],
-  testURL: 'http://localhost/',
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
