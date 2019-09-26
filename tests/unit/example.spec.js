9027ed07207da02ee8e559307e3e0078097af6aa
Jeremy Cook <jeremycook0@icloud.com>
2019-09-26 09:29:31 -0400

    Futher cleanup of examples and added comment that they should be removed.

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,9 +1,13 @@
-import { bar } from '../../src/foo.js';
+import { bar } from '@/foo.js';
 
+/**
+ * This is a stub test, added to ensure that Jest is setup and working.
+ * This test should be removed along with the file /src/foo.js
+ */
 describe('Unit tests', () => {
   it('always passes when run', () => {
-    bar();
-
-    expect(true).toBe(true);
+    const value = bar();
+    expect(value).toBeGreaterThanOrEqual(0);
+    expect(value).toBeLessThanOrEqual(1);
   });
 });
