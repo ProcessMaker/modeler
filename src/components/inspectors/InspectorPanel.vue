08615f6c34a5c7c68cb3f5f8306b25bbcefad382
Robert Komaromi <robertkomaromi@gmail.com>
2019-10-02 14:25:55 -0400

    Reduce inspector label font size by 1px

--- src/components/inspectors/InspectorPanel.vue
+++ src/components/inspectors/InspectorPanel.vue
@@ -198,17 +198,21 @@ export default {
 .inspector-container {
   text-align: left;
   user-select: none;
+
+  .form-group {
+    padding: 0 0.5rem;
+
+    > label {
+      font-size: 0.8125rem;
+    }
+  }
+
+  .inspector-font-size {
+    font-size: 0.875rem;
+  }
 }
 
 .form-accordtion-container {
   padding: 0;
-}
-
-.form-group {
-  padding: 0 0.5rem;
-}
-
-.inspector-font-size {
-  font-size: 0.875rem;
 }
 </style>
