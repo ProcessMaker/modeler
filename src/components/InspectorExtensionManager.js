e67d9b314e8f060986090a7464c5dccdecb1f60d
Tuan N. Truong <t.truong@vehikl.com>
2019-10-03 13:01:29 -0400

    Set advanced accordion to last item in inspector (#719)
    Set advanced accordion last in inspector

---
+++ src/components/InspectorExtensionManager.js
@@ -0,0 +1,36 @@
+export default function registerInspectorExtension(node, config) {
+  const inspectorItems = getInspectorItems(node, config);
+  addInspectorItem(inspectorItems, config);
+  inspectorItems.sort(moveAdvancedAccordionToBottom);
+}
+
+function moveAdvancedAccordionToBottom(inspectorItemA, inspectorItemB) {
+  const isAdvancedAccordion = (inspectorItem) => inspectorItem.name === 'advanced';
+  if (isAdvancedAccordion(inspectorItemA)) {
+    return 1;
+  }
+
+  if (isAdvancedAccordion(inspectorItemB)) {
+    return -1;
+  }
+
+  return 0;
+}
+
+function addInspectorItem(inspectorItems, config) {
+  const nodeIndex = inspectorItems.findIndex(item => {
+    return config.id && config.id === item.id;
+  });
+  if (nodeIndex === -1) {
+    inspectorItems.push(config);
+    return;
+  }
+  inspectorItems[nodeIndex] = config;
+}
+
+function getInspectorItems(node, config) {
+  if (typeof config.container !== 'undefined') {
+    return node.inspectorConfig[0].items;
+  }
+  return node.inspectorConfig[0].items[0].items;
+}
---
+++ tests/unit/inspectorExtensionManager.spec.js
@@ -0,0 +1,69 @@
+import registerInspectorExtension from '@/components/InspectorExtensionManager';
+
+describe('Inspector Extension Manager', function() {
+  let node;
+  let config;
+
+  beforeEach(() => {
+    node = {
+      inspectorConfig: [{
+        items: [
+          {
+            id: 1,
+          },
+          {
+            id: 2,
+          },
+        ],
+      }],
+    };
+    config = {
+      id: 5,
+      container: true,
+      name: 'advanced',
+    };
+  });
+
+  it('should always sort the advanced accordion to the end when the config is a container', function() {
+    registerInspectorExtension(node, config);
+
+    const [lastItem] = node.inspectorConfig[0].items.slice(-1);
+    expect(lastItem).toBe(config);
+  });
+
+  it('should always sort the advanced accordion to the end when the config is not a container', function() {
+    delete config.container;
+    const items = node.inspectorConfig[0].items;
+    node.inspectorConfig[0].items = [{ items }];
+
+    registerInspectorExtension(node, config);
+
+    const [lastItem] = node.inspectorConfig[0].items[0].items.slice(-1);
+    expect(lastItem).toBe(config);
+  });
+
+  it('should update items that have the same id', function() {
+    config.id = 1;
+
+    registerInspectorExtension(node, config);
+
+    const items = node.inspectorConfig[0].items;
+    const [lastItem] = node.inspectorConfig[0].items.slice(-1);
+    expect(items.length).toBe(2);
+    expect(lastItem).toBe(config);
+  });
+
+  it('should only move advanced types to the end', function() {
+    const secondConfig = {
+      id: 6,
+      name: 'Not advanced',
+      container: true,
+    };
+
+    registerInspectorExtension(node, config);
+    registerInspectorExtension(node, secondConfig);
+
+    const [lastItem] = node.inspectorConfig[0].items.slice(-1);
+    expect(lastItem).toBe(config);
+  });
+});
