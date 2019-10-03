4786bd04e9c548a593f7da21b1525c43abe3ed06
Lisa <l.fehr@vehikl.com>
2019-10-03 11:07:54 -0400

    Put interrupting checkbox back into inspector config.

--- src/components/nodes/boundaryMessageEvent/index.js
+++ src/components/nodes/boundaryMessageEvent/index.js
@@ -3,6 +3,17 @@ import boundaryEventConfig from '../boundaryEvent';
 import merge from 'lodash/merge';
 import cloneDeep from 'lodash/cloneDeep';
 
+boundaryEventConfig.inspectorConfig[0].items[0].items.push(
+  {
+    component: 'FormCheckbox',
+    config: {
+      label: 'Interrupting',
+      name: 'cancelActivity',
+      helper: 'Boundary Event Type',
+    },
+  },
+);
+
 export default merge(cloneDeep(boundaryEventConfig), {
   id: 'processmaker-modeler-boundary-message-event',
   component,
