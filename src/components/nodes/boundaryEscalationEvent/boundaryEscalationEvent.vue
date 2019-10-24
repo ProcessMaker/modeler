3c519388253f840ce041cc749c936a9d31d9d099
Lisa Fehr <lisa-fehr@users.noreply.github.com>
2019-10-24 13:24:57 -0400

    Restored method that was accidentally removed (#765)

--- src/components/nodes/boundaryEscalationEvent/boundaryEscalationEvent.vue
+++ src/components/nodes/boundaryEscalationEvent/boundaryEscalationEvent.vue
@@ -1,11 +1,17 @@
 <script>
 import BoundaryEvent from '@/components/nodes/boundaryEvent/boundaryEvent';
 import escalationIcon from '@/assets/boundary-escalation-icon.svg';
+import { isValidBoundaryEscalationEvent } from '@/targetValidationUtils';
 
 export default {
   extends: BoundaryEvent,
   mounted() {
     this.shape.attr('image/xlink:href', escalationIcon);
+  },
+  methods: {
+    isValidBoundaryEventTarget(model) {
+      return isValidBoundaryEscalationEvent(model.component);
+    },
   },
 };
 </script>
