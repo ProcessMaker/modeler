1a5d92905feea7a157c51e0dbc9d409e0a606da9
Tuan Truong <tuan.truong625@gmail.com>
2019-11-07 14:39:10 -0500

    html crown-config

--- src/components/nodes/task/task.vue
+++ src/components/nodes/task/task.vue
@@ -1,5 +1,7 @@
 <template>
-  <div/>
+  <div class="crown-config">
+    <font-awesome-icon class="crown-config__icon" :icon="trashIcon"  @click="removeShape()"/>
+  </div>
 </template>
 
 <script>
@@ -12,11 +14,16 @@ import {markerSize} from '@/mixins/hasMarkers';
 import TaskShape from '@/components/nodes/task/shape';
 import { taskHeight } from './taskConfig';
 import hideLabelOnDrag from '@/mixins/hideLabelOnDrag';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 const labelPadding = 15;
 const topAndBottomMarkersSpace = 2 * markerSize;
 
 export default {
+  components: {
+    FontAwesomeIcon,
+  },
   props: ['graph', 'node', 'id'],
   mixins: [crownConfig, portsConfig, hasMarkers, hideLabelOnDrag],
   data() {
@@ -36,6 +43,9 @@ export default {
   computed: {
     hasTaskMarker() {
       return this.shape.attr('image/xlink:href') ? true : false;
+    },
+    trashIcon() {
+      return faTrash;
     },
   },
   watch: {
@@ -62,6 +72,18 @@ export default {
 
       return this.graph.findModelsInArea(area);
     },
+    removeShape() {
+      this.graph.getConnectedLinks(this.shape).forEach(shape => this.$emit('remove-node', shape.component.node));
+      this.shape.getEmbeddedCells({deep: true}).forEach(cell => {
+        if (cell.component) {
+          this.graph.getConnectedLinks(cell).forEach(shape => this.$emit('remove-node', shape.component.node));
+          this.shape.unembed(cell);
+          this.$emit('remove-node', cell.component.node);
+        }
+      });
+
+      this.$emit('remove-node', this.node);
+    },
   },
   mounted() {
     this.shape = new TaskShape();
@@ -85,3 +107,39 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+$icon-color: #fff;
+$primary-color: #5096db;
+
+.crown-config{
+  background-color: $primary-color;
+  position: absolute;
+  top: 10rem;
+  right: 10rem;
+  z-index: 5;
+  display: flex;
+  justify-content: center;
+  width: auto;
+  height: 1.65rem;
+  border-radius: 5px;
+
+  &::after {
+    background-color: $primary-color;
+    content: '';
+    width: 1.25rem;
+    height: 1.25rem;
+    position: absolute;
+    top: 0.75rem;
+    left: 0.45rem;
+    z-index: -1;
+    transform: rotate(45deg);
+    border-radius: 1px;
+  }
+
+  &__icon {
+    margin: 5px 10px;
+    font-size: 1rem;
+    color: $icon-color;
+  }
+}
+</style>
