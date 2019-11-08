2e5bbcbebec7b6f15c261b3f1d99c66fd8ebf4c2
Lisa <l.fehr@vehikl.com>
2019-11-08 09:54:31 -0500

    Moved new crown config to a component

--- src/components/nodes/task/task.vue
+++ src/components/nodes/task/task.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <div v-if="showCrown" class="crown-config">
-      <font-awesome-icon class="crown-config__icon" :icon="trashIcon"  @click="removeShape()"/>
-    </div>
+    <crown-config
+      v-if="showCrown"
+      :graph="graph"
+      :shape="shape"
+      :node="node"
+      @remove-node="$emit('remove-node', $event)"
+    />
   </div>
 </template>
 
@@ -16,15 +20,14 @@ import {markerSize} from '@/mixins/hasMarkers';
 import TaskShape from '@/components/nodes/task/shape';
 import { taskHeight } from './taskConfig';
 import hideLabelOnDrag from '@/mixins/hideLabelOnDrag';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import CrownConfig from '@/components/crownConfig';
 
 const labelPadding = 15;
 const topAndBottomMarkersSpace = 2 * markerSize;
 
 export default {
   components: {
-    FontAwesomeIcon,
+    CrownConfig,
   },
   props: ['graph', 'node', 'id', 'highlighted'],
   mixins: [crownConfig, portsConfig, hasMarkers, hideLabelOnDrag],
@@ -46,9 +49,6 @@ export default {
   computed: {
     hasTaskMarker() {
       return this.shape.attr('image/xlink:href') ? true : false;
-    },
-    trashIcon() {
-      return faTrash;
     },
   },
   watch: {
@@ -78,18 +78,6 @@ export default {
 
       return this.graph.findModelsInArea(area);
     },
-    removeShape() {
-      this.graph.getConnectedLinks(this.shape).forEach(shape => this.$emit('remove-node', shape.component.node));
-      this.shape.getEmbeddedCells({deep: true}).forEach(cell => {
-        if (cell.component) {
-          this.graph.getConnectedLinks(cell).forEach(shape => this.$emit('remove-node', shape.component.node));
-          this.shape.unembed(cell);
-          this.$emit('remove-node', cell.component.node);
-        }
-      });
-
-      this.$emit('remove-node', this.node);
-    },
   },
   mounted() {
     this.shape = new TaskShape();
@@ -112,39 +100,3 @@ export default {
   },
 };
 </script>
-<style lang="scss">
-$icon-color: #fff;
-$primary-color: #5096db;
-
-.crown-config{
-  background-color: $primary-color;
-  position: absolute;
-  top: 10rem;
-  right: 10rem;
-  z-index: 5;
-  display: flex;
-  justify-content: center;
-  width: auto;
-  height: 1.65rem;
-  border-radius: 5px;
-
-  &::after {
-    background-color: $primary-color;
-    content: '';
-    width: 1.25rem;
-    height: 1.25rem;
-    position: absolute;
-    top: 0.75rem;
-    left: 0.45rem;
-    z-index: -1;
-    transform: rotate(45deg);
-    border-radius: 1px;
-  }
-
-  &__icon {
-    margin: 5px 10px;
-    font-size: 1rem;
-    color: $icon-color;
-  }
-}
-</style>
