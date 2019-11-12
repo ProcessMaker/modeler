fc95c681d0d6933caad99b99228bd8de9645c7f6
Lisa <l.fehr@vehikl.com>
2019-11-12 10:55:27 -0500

    Moved svgs to components

--- src/components/crownConfig.vue
+++ src/components/crownConfig.vue
@@ -1,79 +1,46 @@
 <template>
   <div class="crown-config" :style="style" v-if="showCrown">
-    <svg
+    <sequence-flow-button
       @click="addSequence()"
       class="crown-config__icon"
+      id="sequence-flow-button"
       data-test="sequence-flow-button"
       v-b-tooltip.hover.viewport.d50
       :title="$t('Sequence Flow')"
-      width="19px"
-      height="20px"
-      viewBox="0 0 19 20"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-    >
-      <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="custom/connect-elements" transform="translate(-3.000000, -2.000000)" fill="#000000">
-          <path fill="#fff"
-            d="M18.5,20 L13,20 L13,5 L8,5 L8,7 L3,7 L3,2 L8,2 L8,4 L14,4 L14,19 L18.5,19 L18.5,17 L21.5,19.5 L18.5,22 L18.5,20 Z M4,3 L4,6 L7,6 L7,3 L4,3 Z"
-            id="connect-elements"
-          />
-        </g>
-      </g>
-    </svg>
-    <svg
+    />
+    <message-flow-button
       @click="addMessageFlow()"
       class="crown-config__icon"
+      id="message-flow-button"
       data-test="message-flow-button"
       v-b-tooltip.hover.viewport.d50
       :title="$t('Message Flow')"
-      width="19"
-      height="20"
-      viewBox="0 0 19 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clip-path="url(#clip0)">
-        <circle cx="2.5" cy="2.5" r="2" stroke="#fff"/>
-        <path d="M15.5 20L15.5 17.5V15L18.5 17.5L15.5 20Z" fill="#fff"/>
-        <line x1="4" y1="2.5" x2="7" y2="2.5" stroke="#fff"/>
-        <line x1="8" y1="2.5" x2="11" y2="2.5" stroke="#fff"/>
-        <line x1="10.5" y1="5" x2="10.5" y2="2" stroke="#fff"/>
-        <line x1="10.5" y1="9" x2="10.5" y2="6" stroke="#fff"/>
-        <line x1="10.5" y1="13" x2="10.5" y2="10" stroke="#fff"/>
-        <line x1="10.5" y1="17" x2="10.5" y2="14" stroke="#fff"/>
-        <line x1="10" y1="17.5" x2="13" y2="17.5" stroke="#fff"/>
-        <line x1="14" y1="17.5" x2="17" y2="17.5" stroke="#fff"/>
-      </g>
-      <defs>
-        <clipPath id="clip0">
-          <rect width="19" height="20" fill="white"/>
-        </clipPath>
-      </defs>
-    </svg>
-    <svg
+    />
+    <delete-button
       @click="removeShape()"
       class="crown-config__icon svg-inline--fa fa-trash-alt fa-w-14"
+      id="delete-button"
       data-test="delete-button"
       v-b-tooltip.hover.viewport.d50
       :title="$t('Delete')"
-      aria-hidden="true"
-      data-prefix="fas"
-      data-icon="trash-alt"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
-    >
-      <path fill="#fff" d="M0 84V56c0-13.3 10.7-24 24-24h112l9.4-18.7c4-8.2 12.3-13.3 21.4-13.3h114.3c9.1 0 17.4 5.1 21.5 13.3L312 32h112c13.3 0 24 10.7 24 24v28c0 6.6-5.4 12-12 12H12C5.4 96 0 90.6 0 84zm416 56v324c0 26.5-21.5 48-48 48H80c-26.5 0-48-21.5-48-48V140c0-6.6 5.4-12 12-12h360c6.6 0 12 5.4 12 12zm-272 68c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208z"/>
-    </svg>
+    />
   </div>
 </template>
 
 <script>
 
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import DeleteButton from '@/components/deleteButton';
+import MessageFlowButton from '@/components/messageFlowButton';
+import SequenceFlowButton from '@/components/sequenceFlowButton';
 import pull from 'lodash/pull';
 
 export default {
+  components: {
+    DeleteButton,
+    MessageFlowButton,
+    SequenceFlowButton,
+  },
   props: [
     'paper',
     'graph',
