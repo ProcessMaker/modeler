cf5086ef70af42e84eccb5b887de6f381f0f28d9
Robert Komaromi <robertkomaromi@gmail.com>
2019-11-13 17:01:16 -0500

    Add user icon to task

---
+++ src/components/nodes/userTask/userTask.vue
@@ -0,0 +1,11 @@
+<script>
+import Task from '../task/task';
+import userIcon from '@/assets/user-task.svg';
+
+export default {
+  extends: Task,
+  mounted() {
+    this.shape.attr('image/xlink:href', userIcon);
+  },
+};
+</script>
