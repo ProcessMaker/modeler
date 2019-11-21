e9598fad2a39caf1976f7240d16c1ab5d77bb53b
Robert Komaromi <robertkomaromi@gmail.com>
2019-11-21 10:24:56 -0500

    Pull out shape stacking functions

---
+++ src/components/shapeStackUtils.js
@@ -0,0 +1,54 @@
+import { id as poolId } from './nodes/pool';
+import { id as laneId } from './nodes/poolLane';
+
+export default function ensureShapeIsNotCovered(shape) {
+  if (isPool(shape)) {
+    bringPoolToFront(shape);
+  }
+
+  const parentPool = getElementPool(shape);
+
+  if (parentPool) {
+    bringPoolToFront(parentPool);
+  }
+
+  if (isNotLane(shape) && !isPool(shape)) {
+    bringShapeToFront(shape);
+  }
+}
+
+function isNotLane(shape) {
+  return shape.component.node.type !== laneId;
+}
+
+function bringPoolToFront(poolShape) {
+  bringShapeToFront(poolShape);
+
+  const hasLanes = poolShape.component.node.definition.processRef.laneSets[0];
+  if (!hasLanes) {
+    return;
+  }
+
+  const poolLanes = poolShape
+    .getEmbeddedCells()
+    .filter(cell => cell.component && cell.component.node.type === laneId);
+  const poolLaneElements = poolShape
+    .getEmbeddedCells()
+    .filter(cell => cell.component && cell.component.node.type !== laneId);
+
+  poolLanes.forEach(bringShapeToFront);
+  poolLaneElements.forEach(bringShapeToFront);
+}
+
+function bringShapeToFront(shape) {
+  shape.toFront({ deep: true });
+}
+
+function getElementPool(shape) {
+  return shape.component.node.pool;
+}
+
+function isPool(shape) {
+  return shape.component.node.type === poolId;
+}
+
