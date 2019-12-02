308a00300e818f84dbba571eefab0a2c49de5a32
Jeremy Cook <jeremycook0@icloud.com>
2019-12-02 16:17:48 -0500

    Added test to ensure that pools can be annotated.

--- tests/e2e/specs/TextAnnotation.spec.js
+++ tests/e2e/specs/TextAnnotation.spec.js
@@ -64,4 +64,30 @@ describe('Text Annotation', () => {
         expect(xml).to.contain(expectedXML.trim());
       });
   });
+
+  it('should be able to add a text annotation outside of a pool', () => {
+    const poolPosition = { x: 250, y: 200 };
+    dragFromSourceToDest(nodeTypes.pool, poolPosition);
+
+    const textAnnotationPosition = { x: 400, y: 50 };
+    dragFromSourceToDest(nodeTypes.textAnnotation, textAnnotationPosition);
+
+    connectNodesWithFlow('association-flow-button', textAnnotationPosition, poolPosition);
+
+    const expectedXML = `<bpmn:process id="Process_1" isExecutable="true">
+    <bpmn:startEvent id="node_1" name="Start Event" />
+    <bpmn:textAnnotation id="node_3">
+      <bpmn:text>New Text Annotation</bpmn:text>
+    </bpmn:textAnnotation>
+    <bpmn:association id="node_4" associationDirection="None" sourceRef="node_3" targetRef="node_2" />
+  </bpmn:process>`;
+
+    cy.get('[data-test=downloadXMLBtn]').click();
+    cy.window()
+      .its('xml')
+      .then(xml => xml.trim())
+      .then(xml => {
+        expect(xml).to.contain(expectedXML.trim());
+      });
+  });
 });
