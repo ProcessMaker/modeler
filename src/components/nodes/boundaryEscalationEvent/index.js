e2791082acd2a24a7b9537155a6f04faac11f9cd
Robert Komaromi <robertkomaromi@gmail.com>
2019-12-04 12:55:32 -0500

    Hide boundary escalation event

--- src/components/nodes/boundaryEscalationEvent/index.js
+++ src/components/nodes/boundaryEscalationEvent/index.js
@@ -9,6 +9,7 @@ export const id = 'processmaker-modeler-boundary-escalation-event';
 export default merge(cloneDeep(boundaryEventConfig), {
   id,
   component,
+  control: false,
   label: 'Boundary Escalation Event',
   icon: require('@/assets/toolpanel/boundary-escalation-event.svg'),
   definition(moddle, $t) {
