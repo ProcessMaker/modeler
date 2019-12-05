adf5278fee02eaf596718de5acaf1dfa256677d0
Tuan Truong <tuan.truong625@gmail.com>
2019-12-05 13:51:27 -0500

    Change colour of intermediate elements

--- src/components/nodes/intermediateEvent/intermediateEvent.vue
+++ src/components/nodes/intermediateEvent/intermediateEvent.vue
@@ -60,26 +60,6 @@ export default {
     let bounds = this.node.diagram.bounds;
     this.shape.position(bounds.get('x'), bounds.get('y'));
     this.shape.resize(bounds.get('width'), bounds.get('height'));
-    this.shape.attr({
-      body: {
-        stroke: '#FBBE02',
-        fill: '#FFF4D1',
-      },
-      body2: {
-        stroke: '#FBBE02',
-        fill: '#FFF4D1',
-      },
-      label: {
-        text: this.node.definition.get('name'),
-        refY: '130%',
-      },
-      image: {
-        'ref-x': 5,
-        'ref-y': 5,
-        'width': bounds.get('width') - 10,
-        'height': bounds.get('height') - 10,
-      },
-    });
     this.shape.addTo(this.graph);
     this.shape.component = this;
   },
--- src/components/nodes/intermediateMessageThrowEvent/intermediateMessageThrowEvent.vue
+++ src/components/nodes/intermediateMessageThrowEvent/intermediateMessageThrowEvent.vue
@@ -17,16 +17,6 @@ export default {
   mounted() {
     this.shape.attr('image/xlink:href', intermediateMailSymbol);
     this.shape.attr({
-      body: {
-        stroke: '#000',
-        strokeWidth: 1,
-        fill: '#fff',
-      },
-      body2: {
-        stroke: '#000',
-        strokeWidth: 1,
-        fill: '#fff',
-      },
       image: {
         width: 22,
         height: 20,
