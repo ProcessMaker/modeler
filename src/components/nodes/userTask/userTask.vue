72e0eb3e95a4c08f865688568987409fae010534
Robert Komaromi <robertkomaromi@gmail.com>
2019-12-05 11:55:14 -0500

    Add element dropdown to task components

--- src/components/nodes/userTask/userTask.vue
+++ src/components/nodes/userTask/userTask.vue
@@ -1,3 +1,21 @@
+<template>
+  <crown-config
+    :highlighted="highlighted"
+    :paper="paper"
+    :graph="graph"
+    :shape="shape"
+    :node="node"
+    :nodeRegistry="nodeRegistry"
+    :moddle="moddle"
+    :collaboration="collaboration"
+    :process-node="processNode"
+    :plane-elements="planeElements"
+    :is-rendering="isRendering"
+    :dropdownData="dropdownData"
+    v-on="$listeners"
+  />
+</template>
+
 <script>
 import Task from '../task/task';
 import userIcon from '@/assets/user-task.svg';
@@ -6,6 +24,26 @@ export default {
   extends: Task,
   mounted() {
     this.shape.attr('image/xlink:href', userIcon);
+  },
+  data() {
+    return {
+      dropdownData: [
+        {
+          label: 'Task',
+          nodeType: 'processmaker-modeler-task',
+        },
+        {
+          label: 'Manual Task',
+          nodeType: 'processmaker-modeler-manual-task',
+          dataTest: 'switch-to-manual-task',
+        },
+        {
+          label: 'Script Task',
+          nodeType: 'processmaker-modeler-script-task',
+          dataTest: 'switch-to-script-task',
+        },
+      ],
+    };
   },
 };
 </script>
