2d0da1657ffccc888fa57a9d82c648461927a748
Jeremy Cook <jeremycook0@icloud.com>
2019-12-12 14:42:31 -0500

    Extracted zoom component.

--- src/components/Modeler.vue
+++ src/components/Modeler.vue
@@ -30,37 +30,9 @@
           @load-xml="loadXML"
         />
 
-        <div class="btn-group btn-group-sm mr-2" role="group" aria-label="Zoom controls">
-          <b-button
-            class="btn btn-sm btn-secondary"
-            @click="scale += scaleStep"
-            data-test="zoom-in"
-            v-b-tooltip.hover
-            :title="$t('Zoom In')"
-          >
-            <font-awesome-icon :icon="plusIcon" />
-          </b-button>
-          <b-button
-            class="btn btn-sm btn-secondary"
-            @click="scale = Math.max(minimumScale, scale -= scaleStep)"
-            data-test="zoom-out"
-            v-b-tooltip.hover
-            :title="$t('Zoom Out')"
-          >
-            <font-awesome-icon :icon="minusIcon" />
-          </b-button>
-          <b-button
-            class="btn btn-sm btn-secondary"
-            @click="scale = initialScale"
-            :disabled="scale === initialScale"
-            data-test="zoom-reset"
-            v-b-tooltip.hover
-            :title="$t('Reset to initial scale')"
-          >
-            {{ $t('Reset') }}
-          </b-button>
-          <span class="btn btn-sm btn-secondary scale-value">{{ Math.round(scale*100) }}%</span>
-        </div>
+        <zoom
+          :paper-manager="paperManager"
+        />
 
         <div class="btn-group btn-group-sm mr-2" role="group" aria-label="Additional controls">
           <b-button
@@ -176,11 +148,13 @@ import initAnchor from '@/mixins/linkManager.js';
 import { addIdToNodeAndSetUpDiagramReference, addNodeToProcess, getTargetProcess, isBoundaryEvent } from '@/components/nodeManager';
 import ensureShapeIsNotCovered from '@/components/shapeStackUtils';
 import UndoRedo from '@/components/UndoRedo';
+import Zoom from '@/components/Zoom';
 
 const version = '1.0';
 
 export default {
   components: {
+    Zoom,
     UndoRedo,
     controls,
     InspectorPanel,
@@ -223,10 +197,6 @@ export default {
       parentWidth: null,
       linter: null,
       validationErrors: {},
-      scale: 1,
-      initialScale: 1,
-      minimumScale: 0.2,
-      scaleStep: 0.1,
       miniMapOpen: false,
       panelsCompressed: false,
       isGrabbing: false,
@@ -249,12 +219,6 @@ export default {
 
       document.body.style.cursor = 'auto';
       this.cursor = null;
-    },
-    scale(scale) {
-      this.paperManager.scale = scale;
-      if (scale === this.initialScale) {
-        this.$root.$emit('bv::hide::tooltip');
-      }
     },
     currentXML() {
       this.validateIfAutoValidateIsOn();
