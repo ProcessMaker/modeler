ad8585b98a69fd3d705689f7f5c63a2a26d0525f
Tuan Truong <tuan.truong625@gmail.com>
2019-12-16 10:53:04 -0500

    Refactor status bar

--- src/components/Statusbar.vue
+++ src/components/Statusbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="statusbar border border-bottom-0">
+  <div class="statusbar d-flex align-items-center justify-content-between pr-3 border border-bottom-0">
     <div>
       <slot name="secondary"/>
     </div>
@@ -8,19 +8,3 @@
     </div>
   </div>
 </template>
-
-<style scoped>
-.statusbar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #eeeeee;
-  padding-left: 16px;
-  padding-right: 16px;
-  font-size: 14px;
-  color: #555555;
-  font-weight: normal;
-}
-</style>
-
-
