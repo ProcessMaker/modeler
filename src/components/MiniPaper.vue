5451d8f83c7eaf229c1951f134b6a9c359e8e89f
Tuan Truong <tuan.truong625@gmail.com>
2019-12-16 10:57:33 -0500

    Refactor Mini-paper

--- src/components/MiniPaper.vue
+++ src/components/MiniPaper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mini-paper-container" @click="movePaper" :class="isOpen ? 'opened' : 'closed'">
+  <div class="mini-paper-container position-absolute" @click="movePaper" :class="isOpen ? 'opened' : 'closed'">
     <div ref="miniPaper" class="mini-paper" />
   </div>
 </template>
@@ -52,42 +52,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-$mini-paper-container-top-position: 1rem;
-$mini-paper-container-right-position: 17.5rem;
-$mini-paper-container-right-expanded-position: 2rem;
-$transition: 0.3s;
-
-.mini-paper-container {
-  position: absolute;
-  top: $mini-paper-container-top-position;
-  right: $mini-paper-container-right-position;
-  z-index: 2;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-  border: 1px solid #e9ecef;
-  cursor: pointer;
-
-  .mini-paper {
-    pointer-events: none;
-  }
-
-  &.expanded {
-    right: $mini-paper-container-right-expanded-position;
-  }
-
-  &.closed {
-    opacity: 0;
-    visibility: hidden;
-    transition: opacity $transition ease-out, visibility $transition ease-out;
-  }
-
-  &.opened {
-    visibility: visible;
-    opacity: 1;
-    transition-property: visibility, opacity, right;
-    transition-duration: $transition;
-    transition-timing-function: ease-in, ease-in, linear;
-  }
-}
-</style>
