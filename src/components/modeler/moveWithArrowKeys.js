50d3e496ac714157eea7acf8cb89c52e8b53e1ed
Jeremy Cook <jeremycook0@icloud.com>
2019-12-18 16:22:37 -0500

    Began to add excluded elements that can't be moved with keypresses.

--- src/components/modeler/moveWithArrowKeys.js
+++ src/components/modeler/moveWithArrowKeys.js
@@ -8,13 +8,19 @@ translationAmount.set('Down', [0, moveAmount]);
 translationAmount.set('Left', [-moveAmount, 0]);
 translationAmount.set('Right', [moveAmount, 0]);
 
+const invalidTypes = [
+  'PoolLane',
+  'processmaker.components.nodes.boundaryEvent.Shape',
+];
+
 export default function moveShapeByKeypress(key, shape) {
-  if (!shape) {
+  if (!shape || invalidTypes.includes(shape.get('type'))) {
     return;
   }
   const match = key.match(/^(?:Arrow)?(Up|Down|Left|Right)$/);
   const keyCode = match ? match[1] : undefined;
 
+  const { x, y } = shape.position();
   const [tx, ty] = translationAmount.get(keyCode) || [0, 0];
-  shape.translate(tx, ty);
+  shape.position(x + tx, y + ty);
 }
