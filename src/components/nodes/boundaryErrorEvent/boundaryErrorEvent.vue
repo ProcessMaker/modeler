cf68c614e9b5454277c1b461794014f5a260a57f
Tuan Truong <tuan.truong625@gmail.com>
2019-12-19 10:23:49 -0500

    Remove Boundary error validation

--- src/components/nodes/boundaryErrorEvent/boundaryErrorEvent.vue
+++ src/components/nodes/boundaryErrorEvent/boundaryErrorEvent.vue
@@ -1,24 +1,9 @@
 <script>
 import BoundaryEvent from '@/components/nodes/boundaryEvent/boundaryEvent';
 import errorBoltIcon from '@/assets/boundary-error-event-icon.svg';
-import isValidBoundaryEventTarget from '@/components/nodes/boundaryEvent/validBoundaryEventTargets';
-import { getAttachedErrorBoundaryEvents } from '@/boundaryEventValidation';
 
 export default {
   extends: BoundaryEvent,
-  methods: {
-    doesNotHaveOtherBoundaryEvents(model) {
-      return getAttachedErrorBoundaryEvents(model)
-        .filter(boundaryEvent => {
-          return boundaryEvent !== this.shape;
-        })
-        .length === 0;
-    },
-    isValidBoundaryEventTarget(model) {
-      return isValidBoundaryEventTarget(model.component) &&
-        this.doesNotHaveOtherBoundaryEvents(model);
-    },
-  },
   mounted() {
     this.shape.attr('image/xlink:href', errorBoltIcon);
   },
