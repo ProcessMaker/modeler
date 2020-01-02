fd68078fbfc7a2642b8287982a62def723707784
Robert Komaromi <robertkomaromi@gmail.com>
2020-01-02 11:26:52 -0500

    Change mixin to function

--- src/components/nodes/boundaryMessageEvent/boundaryMessageEvent.vue
+++ src/components/nodes/boundaryMessageEvent/boundaryMessageEvent.vue
@@ -1,15 +1,18 @@
 <script>
 import BoundaryEvent from '@/components/nodes/boundaryEvent/boundaryEvent';
-import intermediateMailIcon from '@/mixins/intermediateMailIcon';
+import getIntermediateMailIconShapeAttributes from '@/mixins/intermediateMailIcon';
 
 export default {
   extends: BoundaryEvent,
-  mixins: [intermediateMailIcon],
   methods: {
     isValidBoundaryEventTarget(model) {
       const component = model.component;
       return component && component.node.definition.$type === 'bpmn:CallActivity';
     },
+  },
+  mounted() {
+    const shapeAttributes = getIntermediateMailIconShapeAttributes();
+    this.shape.attr(shapeAttributes);
   },
 };
 </script>
