8e6907ac40a280e4e2165a7b9a212a7d9f4f8deb
Lisa <l.fehr@vehikl.com>
2020-01-09 10:18:42 -0500

    Refactor fix for failing test

--- src/components/crown/utils.js
+++ src/components/crown/utils.js
@@ -2,9 +2,8 @@ export function removeFlows(graph, shape) {
   graph.getConnectedLinks(shape).forEach(shape => this.$emit('remove-node', shape.component.node));
   shape.getEmbeddedCells({ deep: true })
     .filter(cell => cell.component)
-    .flatMap(cell => graph.getConnectedLinks(cell))
     .forEach(cell => {
-      this.$emit('remove-node', shape.component.node);
+      graph.getConnectedLinks(cell).forEach(shape => this.$emit('remove-node', shape.component.node));
       shape.unembed(cell);
       this.$emit('remove-node', cell.component.node);
     });
