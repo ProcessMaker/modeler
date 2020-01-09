f08fe9644a07c1bdf7cb88fb629e73f8a1ea1c4f
Lisa <l.fehr@vehikl.com>
2020-01-09 09:53:41 -0500

    Refactor

--- src/components/crown/utils.js
+++ src/components/crown/utils.js
@@ -1,10 +1,11 @@
 export function removeFlows(graph, shape) {
-  graph.getConnectedLinks(this.shape).forEach(shape => this.$emit('remove-node', shape.component.node));
-  shape.getEmbeddedCells({ deep: true }).forEach(cell => {
-    if (cell.component) {
-      graph.getConnectedLinks(cell).forEach(shape => this.$emit('remove-node', shape.component.node));
+  graph.getConnectedLinks(shape).forEach(shape => this.$emit('remove-node', shape.component.node));
+  shape.getEmbeddedCells({ deep: true })
+    .filter(cell => cell.component)
+    .flatMap(cell => graph.getConnectedLinks(cell))
+    .forEach(cell => {
+      this.$emit('remove-node', shape.component.node);
       shape.unembed(cell);
       this.$emit('remove-node', cell.component.node);
-    }
-  });
+    });
 }
