f973cdb5eb6836743f099a9851da682d184bbeb7
Robert Komaromi <chipit24@users.noreply.github.com>
2020-01-29 14:35:49 -0500

    Add intermediate signal catch event (#1074)

---
+++ src/components/nodes/intermediateSignalCatchEvent/intermediateSignalCatchEvent.vue
@@ -0,0 +1,19 @@
+<script>
+import IntermediateEvent from '@/components/nodes/intermediateEvent/intermediateEvent';
+import intermediateSignalCatchEventSymbol from '@/assets/intermediate-signal-catch-event.svg';
+
+export default {
+  extends: IntermediateEvent,
+  mounted() {
+    this.shape.attr({
+      image: {
+        'ref-x': 7,
+        'ref-y': 5,
+        'xlink:href': intermediateSignalCatchEventSymbol,
+        width: 22,
+        height: 22,
+      },
+    });
+  },
+};
+</script>
