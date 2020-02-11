0a4b8b480568303877caaf87cd772aa265419991
Robert Komaromi <robertkomaromi@gmail.com>
2020-02-11 11:25:24 -0500

    Fix issue with reference in logging script

--- tests/e2e/support/printLogsToTerminal.js
+++ tests/e2e/support/printLogsToTerminal.js
@@ -1,10 +1,15 @@
 /* This file allows console.log's to be displayed in the terminal to help with debugging
- * tests run on the CI. See https://github.com/cypress-io/cypress/issues/3199. */
+ * tests run on the CI. See https://github.com/cypress-io/cypress/issues/3199#issuecomment-466593084. */
 
 let logs = [];
 
 Cypress.on('window:before:load', window => {
   const docIframe = window.parent.document.getElementById('Your App: \'modeler\'');
+
+  if (!docIframe) {
+    return;
+  }
+
   const appWindow = docIframe.contentWindow;
 
   ['log', 'info', 'error', 'warn', 'debug'].forEach(consoleMethod => {
