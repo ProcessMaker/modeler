6e24123f6c4fae4802ad5e642c31ee4d762336de
Robert Komaromi <robertkomaromi@gmail.com>
2020-02-11 10:44:09 -0500

    Print console logs to console for CI
    To help debug CI test failures.

---
+++ tests/e2e/support/printLogsToTerminal.js
@@ -0,0 +1,28 @@
+/* This file allows console.log's to be displayed in the terminal to help with debugging
+ * tests run on the CI. See https://github.com/cypress-io/cypress/issues/3199. */
+
+let logs = [];
+
+Cypress.on('window:before:load', window => {
+  const docIframe = window.parent.document.getElementById('Your App: \'modeler\'');
+  const appWindow = docIframe.contentWindow;
+
+  ['log', 'info', 'error', 'warn', 'debug'].forEach(consoleMethod => {
+    appWindow.console[consoleMethod] = function(...args) {
+      logs.push(args.join(' '));
+    };
+  });
+});
+
+Cypress.mocha.getRunner().on('test', () => {
+  logs = [];
+});
+
+Cypress.on('fail', error => {
+  error.stack += '\n\nConsole Logs:\n=============\n';
+  error.stack += logs.join('\n');
+
+  logs = [];
+
+  throw error;
+});
