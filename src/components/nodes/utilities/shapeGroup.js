6513247d8efba8cd2eb9671de2bc605871c5ab37
Gerrie Swart <g.swart@vehikl.com>
2020-02-12 16:32:34 -0500

    Add shape group options logic

--- src/components/nodes/utilities/shapeGroup.js
+++ src/components/nodes/utilities/shapeGroup.js
@@ -1,3 +1,24 @@
+import {
+  shapeBottom,
+  shapeCenterX,
+  shapeCenterY,
+  shapeLeft,
+  shapeRight,
+  shapeTop,
+} from '@/components/nodes/utilities/shapeMetrics';
+import {
+  alignBottom,
+  alignLeft,
+  alignRight,
+  alignTop,
+  centerHorizontally,
+  centerVertically,
+} from '@/components/nodes/utilities/align';
+import {
+  distributeHorizontalSpacingEvenly,
+  distributeVerticalCentersEvenly,
+} from '@/components/nodes/utilities/distribute';
+
 /**
  * Utility functions that operate on a collection of shapes
  */
@@ -24,5 +45,38 @@ export function getBoundingBox(shapes) {
     height,
     vMiddle: minY + (height / 2),
     hMiddle: minX + (width / 2),
+  };
+}
+
+export function getShapesOptions(shapes) {
+  const bounds = getBoundingBox(shapes);
+  const canAlign = shapes.length > 1;
+  return {
+    can: {
+      align: {
+        left: canAlign && shapes.some(shape => shapeLeft(shape) !== bounds.left),
+        horizontalCenter: canAlign && shapes.some(shape => shapeCenterX(shape) !== bounds.hMiddle),
+        right: canAlign && shapes.some(shape => shapeRight(shape) !== bounds.right),
+        bottom: canAlign && shapes.some(shape => shapeBottom(shape) !== bounds.bottom),
+        verticalCenter: canAlign && shapes.some(shape => shapeCenterY(shape) !== bounds.vMiddle),
+        top: canAlign && shapes.some(shape => shapeTop(shape) !== bounds.top),
+      },
+      distribute: {
+        horizontally: canAlign && shapes.length > 2,
+        vertically: canAlign && shapes.length > 2,
+      },
+    },
+    align: {
+      left: () => alignLeft(shapes),
+      horizontalCenter: () => centerHorizontally(shapes),
+      right: () => alignRight(shapes),
+      bottom: () => alignBottom(shapes),
+      verticalCenter: () => centerVertically(shapes),
+      top: () => alignTop(shapes),
+    },
+    distribute: {
+      horizontally: () => distributeHorizontalSpacingEvenly(shapes),
+      vertically: () => distributeVerticalCentersEvenly(shapes),
+    },
   };
 }
