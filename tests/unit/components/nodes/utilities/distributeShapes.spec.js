4a033d301fb76de15b59c8e7a1177d2028051c44
Gerrie Swart <g.swart@vehikl.com>
2020-02-12 10:14:46 -0500

    Assert some basic distribution logic

---
+++ tests/unit/components/nodes/utilities/distributeShapes.spec.js
@@ -0,0 +1,79 @@
+import {
+  distributeHorizontalCentersEvenly,
+  distributeHorizontalSpacingEvenly,
+  distributeVerticalCentersEvenly,
+} from '@/components/nodes/utilities/distribute';
+import { dummyShape } from '../../../utilities/dummies';
+import toHaveBeenProgrammaticallyMoved from '../../../customMatchers/toHaveBeenProgrammaticallyMoved';
+import toHaveBeenProgrammaticallyMovedBy from '../../../customMatchers/toHaveBeenProgrammaticallyMovedBy';
+
+/**
+ * @type {Function}
+ */
+const expect = require('expect');
+expect.extend({ toHaveBeenProgrammaticallyMoved, toHaveBeenProgrammaticallyMovedBy });
+
+describe('Shape Distribution', () => {
+  it('can distribute vertical centers', () => {
+    const shapes = [
+      dummyShape(0, 0, 100, 50),
+      dummyShape(100, 50, 100, 50),
+      dummyShape(100, 100, 100, 50),
+      dummyShape(200, 600, 100, 50),
+    ];
+
+    distributeVerticalCentersEvenly(shapes);
+
+    expect(shapes[0]).not.toHaveBeenProgrammaticallyMoved();
+    expect(shapes[1]).toHaveBeenProgrammaticallyMovedBy(0, 150);
+    expect(shapes[2]).toHaveBeenProgrammaticallyMovedBy(0, 300);
+    expect(shapes[3]).not.toHaveBeenProgrammaticallyMoved();
+  });
+
+  it('only distributes three or more items', () => {
+    const shapes = [
+      dummyShape(0, 0, 100, 50),
+      dummyShape(200, 600, 100, 50),
+    ];
+
+    distributeVerticalCentersEvenly(shapes);
+    distributeHorizontalCentersEvenly(shapes);
+    distributeHorizontalSpacingEvenly(shapes);
+
+    expect(shapes[0]).not.toHaveBeenProgrammaticallyMoved();
+    expect(shapes[1]).not.toHaveBeenProgrammaticallyMoved();
+  });
+
+  it('can distribute horizontal centers', () => {
+    const shapes = [
+      dummyShape(0, 0, 100, 50),
+      dummyShape(100, 0, 50, 50),
+      dummyShape(150, 0, 100, 50),
+      dummyShape(300, 0, 100, 50),
+    ];
+
+    distributeHorizontalCentersEvenly(shapes);
+
+    expect(shapes[0]).not.toHaveBeenProgrammaticallyMoved();
+    expect(shapes[1]).toHaveBeenProgrammaticallyMovedBy(25, 0);
+    expect(shapes[2]).toHaveBeenProgrammaticallyMovedBy(50, 0);
+    expect(shapes[3]).not.toHaveBeenProgrammaticallyMoved();
+  });
+
+  it('can distribute horizontal spacing', () => {
+    const shapes = [
+      dummyShape(0, 0, 100, 50),
+      dummyShape(100, 0, 50, 50),
+      dummyShape(150, 0, 100, 50),
+      dummyShape(400, 0, 100, 50),
+    ];
+
+    distributeHorizontalSpacingEvenly(shapes);
+
+    expect(shapes[0]).not.toHaveBeenProgrammaticallyMoved();
+    expect(shapes[1]).toHaveBeenProgrammaticallyMovedBy(50, 0);
+    expect(shapes[2]).toHaveBeenProgrammaticallyMovedBy(100, 0);
+    expect(shapes[3]).not.toHaveBeenProgrammaticallyMoved();
+  });
+});
+
