8df3900937dbb87627565fd24b76b036217d7666
Gerrie Swart <johnblackspear@users.noreply.github.com>
2020-02-12 11:10:55 -0500

    Feature/shape metrics (#1108)
    * Assert basic behaviour for shape metric functions
    
    * Add basic shape metrics
    
    * Factory for a dummy shape that we can assert things against
    
    * Remove module.exports

---
+++ src/components/nodes/utilities/shapeMetrics.js
@@ -0,0 +1,6 @@
+export const shapeBottom = (shape) => shape.position().y + shape.size().height;
+export const shapeLeft = (shape) => shape.position().x;
+export const shapeCenterX = (shape) => shape.position().x + (shape.size().width / 2);
+export const shapeCenterY = (shape) => shape.position().y + (shape.size().height / 2);
+export const shapeRight = (shape) => shape.position().x + shape.size().width;
+export const shapeTop = (shape) => shape.position().y;
