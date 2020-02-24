9ae579b1a5b70ecaee3e3256be9fe1ff7fcbf685
Robert Komaromi <robertkomaromi@gmail.com>
2020-02-24 10:32:02 -0500

    Fix timer event copy method

--- src/components/nodes/timerEventNode.js
+++ src/components/nodes/timerEventNode.js
@@ -3,8 +3,15 @@ import Node from './node';
 export default class TimerEventNode extends Node {
   static timerPropertyKeys = ['timeDuration', 'timeDate', 'timeCycle'];
 
-  clone(...args) {
-    const clonedNode = super.clone(...args);
+  clone(nodeRegistry, moddle, $t) {
+    const definition = nodeRegistry[this.type].definition(moddle, $t);
+    const diagram = nodeRegistry[this.type].diagram(moddle);
+    const clonedNode = new TimerEventNode(this.type, definition, diagram);
+
+    clonedNode.id = null;
+    Node.diagramPropertiesToCopy.forEach(prop => clonedNode.diagram.bounds[prop] = this.diagram.bounds[prop]);
+    clonedNode.definition.name = this.definition.name;
+
     const timerKey = TimerEventNode.timerPropertyKeys.find(key => this.definition.eventDefinitions[0][key]);
 
     if (!clonedNode.definition.eventDefinitions[0][timerKey]) {
