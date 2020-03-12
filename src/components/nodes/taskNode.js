31472ecaed70f6ab7d960d99ff6a325429df0151
Lisa <l.fehr@vehikl.com>
2020-03-12 14:44:22 -0400

    Temp fix to not erroring when copying an imported task

--- src/components/nodes/taskNode.js
+++ src/components/nodes/taskNode.js
@@ -2,9 +2,14 @@ import Node from './node';
 import cloneDeep from 'lodash/cloneDeep';
 
 export default class TaskNode extends Node {
-  clone() {
+  clone(nodeRegistry, moddle, $t) {
+    let definition = nodeRegistry[this.type].definition(moddle, $t);
     const diagramClone = cloneDeep(this.diagram);
-    const clonedNode = new TaskNode(this.type, diagramClone.bpmnElement, diagramClone);
+
+    if (diagramClone.bpmnElement) {
+      definition = diagramClone.bpmnElement;
+    }
+    const clonedNode = new TaskNode(this.type, definition, diagramClone);
     clonedNode.id = null;
     Node.diagramPropertiesToCopy.forEach(prop => clonedNode.diagram.bounds[prop] = this.diagram.bounds[prop]);
     clonedNode.definition.name = this.definition.name;
