a8d478b09065c057027ef426702be461ac8b6ca6
Robert Komaromi <robertkomaromi@gmail.com>
2020-03-19 11:59:30 -0400

    Load SVGs via data uri

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,27 @@ const yargs = require('yargs');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg');
+
+    // clear all existing loaders.
+    // if you don't do this, the loader below will be appended to
+    // existing loaders of the rule.
+    svgRule.uses.clear();
+
+    // add replacement loader(s)
+    svgRule
+      .use('url-loader')
+      .loader('url-loader');
+
+    // config.module
+    //   .rule('svg')
+    //   .use('url-loader')
+    //   .loader('url-loader')
+    //   .tap(() => {
+    //     return { limit: false };
+    //   });
+  },
   css: {
     extract: false,
     loaderOptions: {
