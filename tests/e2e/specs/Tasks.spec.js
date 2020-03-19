8afd84647defce89fcf5eb1eb3197056eed7b704
Lisa <l.fehr@vehikl.com>
2020-03-19 14:43:43 -0400

    Updated test

--- tests/e2e/specs/Tasks.spec.js
+++ tests/e2e/specs/Tasks.spec.js
@@ -36,14 +36,15 @@ describe('Tasks', () => {
   });
 
   it('Can create sub process with config', () => {
-    const subProcessPosition = { x: 250, y: 250 };
     const processName = 'Process with multiple start events';
+    const subProcessPosition = { x: 250, y: 250 };
+    const subProcessName = 'Sub Process';
     const startEventName = 'Start Event Two';
     const encodedConfig = JSON.stringify({
       calledElement: 'Subprocess1-5',
       processId: 5,
       startEvent: 'node_10',
-      name: `${processName} (${startEventName})`,
+      name: `${subProcessName}`,
     }).replace(/"/g, '&#34;');
 
     addNodeTypeToPaper(subProcessPosition, nodeTypes.task, 'switch-to-sub-process');
@@ -66,7 +67,7 @@ describe('Tasks', () => {
       .contains(startEventName)
       .click();
 
-    assertDownloadedXmlContainsExpected(`<bpmn:callActivity id="node_3" name="${processName} (${startEventName})" calledElement="Subprocess1-5" pm:config="${encodedConfig}" />`);
+    assertDownloadedXmlContainsExpected(`<bpmn:callActivity id="node_3" name="${subProcessName}" calledElement="Subprocess1-5" pm:config="${encodedConfig}" />`);
   });
 
   it('Can switch task type when initially added', () => {
