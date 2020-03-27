5f27bb7af1cbfc65f92abcf13a43464459e5d32a
Lisa <l.fehr@vehikl.com>
2020-03-27 16:54:21 -0400

    Simplified tests

--- tests/unit/keepOriginalName.spec.js
+++ tests/unit/keepOriginalName.spec.js
@@ -2,82 +2,26 @@ import { keepOriginalName} from '@/components/modeler/modelerUtils';
 import Node from '@/components/nodes/node';
 
 describe('Switch Types', () => {
-  it('Should keep unique Task name', () => {
+  const nodeTypes = [
+    ['Form Task', 'processmaker-modeler-script-task'],
+    ['Start Event', 'processmaker-modeler-start-timer-event'],
+    ['End Event', 'processmaker-modeler-message-end-event'],
+    ['Intermediate Message Catch Event', 'processmaker-modeler-intermediate-catch-timer-event'],
+    ['Exclusive Gateway', 'processmaker-modeler-parallel-gateway'],
+  ];
+
+  it.each(nodeTypes)('Should keep unique %s name', (name, type) => {
     const node = new Node(
-      'processmaker-modeler-script-task',
-      { id: 'test', name: 'Unique Task Name' },
+      type,
+      { id: 'test', name: `Unique ${name} Name` },
       {},
     );
     expect(keepOriginalName(node)).toBe(true);
   });
-  it('Should switch default Task name', () => {
+  it.each(nodeTypes)('Should switch default %s name', (name, type) => {
     const node = new Node(
-      'processmaker-modeler-script-task',
-      { id: 'test', name: 'Form Task' },
-      {},
-    );
-    expect(keepOriginalName(node)).toBe(false);
-  });
-  it('Should keep unique Start name', () => {
-    const node = new Node(
-      'processmaker-modeler-start-timer-event',
-      { id: 'test', name: 'Unique Start Event Name' },
-      {},
-    );
-    expect(keepOriginalName(node)).toBe(true);
-  });
-  it('Should switch default Start name', () => {
-    const node = new Node(
-      'processmaker-modeler-start-timer-event',
-      { id: 'test', name: 'Start Event' },
-      {},
-    );
-    expect(keepOriginalName(node)).toBe(false);
-  });
-  it('Should keep unique End name', () => {
-    const node = new Node(
-      'processmaker-modeler-message-end-event',
-      { id: 'test', name: 'Unique End Event Name' },
-      {},
-    );
-    expect(keepOriginalName(node)).toBe(true);
-  });
-  it('Should switch default End name', () => {
-    const node = new Node(
-      'processmaker-modeler-message-end-event',
-      { id: 'test', name: 'End Event' },
-      {},
-    );
-    expect(keepOriginalName(node)).toBe(false);
-  });
-  it('Should keep unique Intermediate name', () => {
-    const node = new Node(
-      'processmaker-modeler-intermediate-catch-timer-event',
-      { id: 'test', name: 'Unique Intermediate Event Name' },
-      {},
-    );
-    expect(keepOriginalName(node)).toBe(true);
-  });
-  it('Should switch default Intermediate name', () => {
-    const node = new Node(
-      'processmaker-modeler-intermediate-catch-timer-event',
-      { id: 'test', name: 'Intermediate Message Catch Event' },
-      {},
-    );
-    expect(keepOriginalName(node)).toBe(false);
-  });
-  it('Should keep unique Gateway name', () => {
-    const node = new Node(
-      'processmaker-modeler-parallel-gateway',
-      { id: 'test', name: 'Unique Gateway Name' },
-      {},
-    );
-    expect(keepOriginalName(node)).toBe(true);
-  });
-  it('Should switch default Gateway name', () => {
-    const node = new Node(
-      'processmaker-modeler-parallel-gateway',
-      { id: 'test', name: 'Exclusive Gateway' },
+      type,
+      { id: 'test', name },
       {},
     );
     expect(keepOriginalName(node)).toBe(false);
