52379c1624f3f322bee7a6dceafb527266d6cf13
Lisa <l.fehr@vehikl.com>
2020-03-30 13:54:40 -0400

    Added to other icons

--- src/components/nodes/signalStartEvent/signalStartEvent.vue
+++ src/components/nodes/signalStartEvent/signalStartEvent.vue
@@ -1,15 +1,21 @@
 <script>
 import startEvent from '../startEvent/startEvent';
-import signalStartEventIcon from '@/assets/signal-start-event.svg';
+import signalStartEventIcon from '!!svg-inline-loader!@/assets/signal-start-event.svg';
+import updateIconColor from '@/mixins/updateIconColor';
 
 export default {
   extends: startEvent,
+  mixins: [updateIconColor],
+  data() {
+    return {
+      nodeIcon: signalStartEventIcon,
+    };
+  },
   mounted() {
     this.shape.attr({
       image: {
         'ref-x': 5,
         'ref-y': 3,
-        'xlink:href': signalStartEventIcon,
       },
     });
   },
