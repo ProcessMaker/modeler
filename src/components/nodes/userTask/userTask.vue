b0a46e023aad6b81f29779f85ece9d80bd5679a4
Robert Komaromi <robertkomaromi@gmail.com>
2020-03-30 10:50:45 -0400

    WIP

--- src/components/nodes/userTask/userTask.vue
+++ src/components/nodes/userTask/userTask.vue
@@ -2,15 +2,20 @@
 import Task from '../task/task';
 import userIcon from '!!svg-inline-loader!@/assets/user-task.svg';
 
-export default {
-  extends: Task,
-  mounted() {
-    this.shape.attr('image/xlink:href', encodeAsDataUri(userIcon));
-  },
-};
-
 const encodeAsDataUri = (svgString) => {
   const base64 = btoa(svgString);
   return `data:image/svg+xml;base64,${base64}`;
+};
+
+export default {
+  extends: Task,
+  mounted() {
+    const svg = (new DOMParser()).parseFromString(userIcon, 'text/xml');
+    svg.querySelectorAll('*[fill]:not([fill="none"])').forEach(svgNode => {
+      svgNode.setAttribute('fill', 'blue');
+    });
+
+    this.shape.attr('image/xlink:href', encodeAsDataUri(userIcon));
+  },
 };
 </script>
