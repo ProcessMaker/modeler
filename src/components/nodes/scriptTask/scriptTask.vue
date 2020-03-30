2254f2e0fbd5a3bc645909812c6168dccf803e47
Gerrie Swart <g.swart@vehikl.com>
2020-03-30 13:39:22 -0400

    Use updateIconColor mixin

--- src/components/nodes/scriptTask/scriptTask.vue
+++ src/components/nodes/scriptTask/scriptTask.vue
@@ -1,20 +1,15 @@
 <script>
 import TaskComponent from '@/components/nodes/task/task';
 import scriptIcon from '!!svg-inline-loader!@/assets/script.svg';
-import coloredIcon from '@/components/iconColors';
+import updateIconColor from '@/mixins/updateIconColor';
 
 export default {
   extends: TaskComponent,
-  watch: {
-    'node.definition.color': {
-      handler() {
-        this.shape.attr('image/xlink:href', coloredIcon(scriptIcon, this.node.definition.get('color')));
-      },
-      deep: true,
-    },
-  },
-  mounted() {
-    this.shape.attr('image/xlink:href', coloredIcon(scriptIcon, this.node.definition.get('color')));
+  mixins: [updateIconColor],
+  data() {
+    return {
+      nodeIcon: scriptIcon,
+    };
   },
 };
 </script>
--- src/components/nodes/userTask/userTask.vue
+++ src/components/nodes/userTask/userTask.vue
@@ -1,20 +1,15 @@
 <script>
 import Task from '../task/task';
 import userIcon from '!!svg-inline-loader!@/assets/user-task.svg';
-import coloredIcon from '@/components/iconColors';
+import updateIconColor from '@/mixins/updateIconColor';
 
 export default {
   extends: Task,
-  watch: {
-    'node.definition.color': {
-      handler() {
-        this.shape.attr('image/xlink:href', coloredIcon(userIcon, this.node.definition.get('color')));
-      },
-      deep: true,
-    },
-  },
-  mounted() {
-    this.shape.attr('image/xlink:href', coloredIcon(userIcon, this.node.definition.get('color')));
+  mixins: [updateIconColor],
+  data() {
+    return {
+      nodeIcon: userIcon,
+    };
   },
 };
 </script>
