d81ba7c9626f1a12172723cbf2943ecf82c48244
Gerrie Swart <g.swart@vehikl.com>
2020-03-30 10:22:17 -0400

    WIP: inline image data URI encoding for now

--- src/components/nodes/userTask/userTask.vue
+++ src/components/nodes/userTask/userTask.vue
@@ -5,7 +5,12 @@ import userIcon from '!!svg-inline-loader!@/assets/user-task.svg';
 export default {
   extends: Task,
   mounted() {
-    this.shape.attr('image/xlink:href', userIcon);
+    this.shape.attr('image/xlink:href', encodeAsDataUri(userIcon));
   },
+};
+
+const encodeAsDataUri = (svgString) => {
+  const base64 = btoa(svgString);
+  return `data:image/svg+xml;base64,${base64}`;
 };
 </script>
