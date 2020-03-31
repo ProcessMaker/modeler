89824363311d55d212c9b9527901737bd716df3f
David Callizaya <davidcallizaya@gmail.com>
2020-03-31 18:15:32 -0400

    Update Intermediate Signal Throw Event test

--- tests/e2e/specs/IntermediateSignalThrowEvent.spec.js
+++ tests/e2e/specs/IntermediateSignalThrowEvent.spec.js
@@ -1,10 +1,11 @@
-import { addNodeTypeToPaper, assertDownloadedXmlContainsExpected } from '../support/utils';
+import { addNodeTypeToPaper, assertDownloadedXmlContainsExpected, typeIntoTextInput } from '../support/utils';
 import { nodeTypes } from '../support/constants';
 
 describe('Intermediate Signal Throw Event', () => {
   it('Can create intermediate signal throw event', () => {
     const intermediateSignalThrowEventPosition = { x: 250, y: 250 };
     addNodeTypeToPaper(intermediateSignalThrowEventPosition, nodeTypes.intermediateCatchEvent, 'switch-to-intermediate-signal-throw-event');
+    typeIntoTextInput('[name=signalRef]', 'node_3_signal');
 
     assertDownloadedXmlContainsExpected(`
       <bpmn:intermediateThrowEvent id="node_3" name="Intermediate Signal Throw Event">
