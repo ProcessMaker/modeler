78ce98827ede77e8316885bf0495eb644acd090e
Robert Komaromi <robertkomaromi@gmail.com>
2020-04-01 10:58:12 -0400

    Copy filtered list of definition keys

--- src/components/nodes/taskNode.js
+++ src/components/nodes/taskNode.js
@@ -1,18 +1,20 @@
 import Node from './node';
-import cloneDeep from 'lodash/cloneDeep';
 
 export default class TaskNode extends Node {
-  clone(nodeRegistry, moddle, $t) {
-    let definition = nodeRegistry[this.type].definition(moddle, $t);
-    const diagramClone = cloneDeep(this.diagram);
+  static definitionPropertiesToNotCopy = ['$type', 'id'];
 
-    if (diagramClone.bpmnElement) {
-      definition = diagramClone.bpmnElement;
-    }
-    const clonedNode = new TaskNode(this.type, definition, diagramClone);
+  clone(nodeRegistry, moddle, $t) {
+    const definition = nodeRegistry[this.type].definition(moddle, $t);
+    const diagram = nodeRegistry[this.type].diagram(moddle);
+    const clonedNode = new TaskNode(this.type, definition, diagram);
+
     clonedNode.id = null;
     Node.diagramPropertiesToCopy.forEach(prop => clonedNode.diagram.bounds[prop] = this.diagram.bounds[prop]);
-    clonedNode.definition.name = this.definition.name;
+    Object.keys(this.definition)
+      .filter(key => !TaskNode.definitionPropertiesToNotCopy.includes(key))
+      .forEach(key => {
+        clonedNode.definition[key] = this.definition[key];
+      });
 
     return clonedNode;
   }
