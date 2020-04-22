df86e3d3e3acc1fa3647e14a11bb71ad85a32c14
Robert Komaromi <robertkomaromi@gmail.com>
2020-04-22 16:13:36 -0400

    Fix node color test

--- src/store.js
+++ src/store.js
@@ -74,12 +74,12 @@ export default new Vuex.Store({
       });
     },
     updateNodeProp(state, { node, key, value }) {
+      node.definition.set(key, value);
+
       if (value === undefined) {
         Vue.delete(node.definition, key);
         return;
       }
-
-      node.definition.set(key, value);
 
       makeDefinitionPropertyReactive(node.definition, key, value);
     },
