a08bfe4fbdbf16e3ea3b3f4b450f566e149df8bf
Gerrie Swart <johnblackspear@users.noreply.github.com>
2020-05-01 13:53:22 -0400

    Add redo-alternative FA icon to color clear button (#1223)

--- src/components/crown/crownButtons/crownColorDropdown.vue
+++ src/components/crown/crownButtons/crownColorDropdown.vue
@@ -23,7 +23,7 @@
 
       <button
         type="button"
-        class="color-button"
+        class="color-button p-0 fa fa-redo-alt"
         data-test="clear-color"
         @click="selectedColor = null"
       />
