e124a5a1b2ba4ba0e2f62fd7ca5a3da6fd24477d
Vehikl Team <go@vehikl.com>
2020-10-08 14:51:40 -0400

    extract helpers to express intent
     
    Co-Authored-By: Robert Komaromi <chipit24@users.noreply.github.com>
    Co-Authored-By: Gerrie Swart <johnblackspear@users.noreply.github.com>
    Co-Authored-By: Justin Struk <jstruk@users.noreply.github.com>

--- tests/e2e/specs/SwitchingElements.spec.js
+++ tests/e2e/specs/SwitchingElements.spec.js
@@ -7,31 +7,38 @@ import {
 } from '../support/utils';
 import {nodeTypes} from '../support/constants';
 
+function addFlowExpression(flowExpression, gatewayPosition) {
+  getElementAtPosition(gatewayPosition)
+    .then(getLinksConnectedToElement)
+    .then($links => $links[0])
+    .click({ force: true });
+
+  cy.contains('Expression')
+    .next('input')
+    .clear()
+    .type(flowExpression);
+}
+
+function changeGatewayTypeTo(newType, gatewayPosition) {
+  getElementAtPosition(gatewayPosition).click();
+
+  cy.get('[data-test=select-type-dropdown]').click();
+  cy.get(`[data-test=switch-to-${newType}]`).click();
+  modalConfirm();
+}
+
 describe('Switching elements', () => {
   it('Switching a exclusive gateway to a parallel gateway should remove conditions from flows', () => {
     const gatewayPosition = {x: 300, y: 150};
     const taskPosition = {x: 450, y: 150};
     dragFromSourceToDest(nodeTypes.exclusiveGateway, gatewayPosition);
     dragFromSourceToDest(nodeTypes.task, taskPosition);
-
     connectNodesWithFlow('sequence-flow-button', gatewayPosition, taskPosition);
 
-    getElementAtPosition(gatewayPosition)
-      .then(getLinksConnectedToElement)
-      .then($links => $links[0])
-      .click({ force: true });
-
     const flowExpression = '1234 == 1234';
-    cy.contains('Expression')
-      .next('input')
-      .clear()
-      .type(flowExpression);
 
-    getElementAtPosition(gatewayPosition).click();
-
-    cy.get('[data-test=select-type-dropdown]').click();
-    cy.get('[data-test=switch-to-parallel-gateway]').click();
-    modalConfirm();
+    addFlowExpression(flowExpression, gatewayPosition);
+    changeGatewayTypeTo('parallel-gateway', gatewayPosition);
 
     assertDownloadedXmlDoesNotContainExpected(flowExpression);
   });
