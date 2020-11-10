e6ae69f1036c423821bb3d631e00086a6508c865
Marco Antonio Nina Mena <marcoantonina@gmail.com>
2020-11-10 16:55:56 -0400

    add variable request setting

---
+++ src/components/inspectors/requestVariableSettings.js
@@ -0,0 +1,8 @@
+const requestVariableSettings = {
+  label: 'Request Variable',
+  name: 'request_variable',
+  helper: 'Enter the request variable that will store the signal payload',
+  validation: 'required',
+};
+
+export default requestVariableSettings;
