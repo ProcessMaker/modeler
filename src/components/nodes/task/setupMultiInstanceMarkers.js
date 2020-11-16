b2463fd6fde58be9e6a6ac52ff38ed0f8033a046
vehikl-jacare <vehikl-jacare@users.noreply.github.com>
2020-11-16 14:54:00 -0500

    refactor multi instance marker logic
    Co-Authored-By: Robert Komaromi <chipit24@users.noreply.github.com>
    Co-Authored-By: Gerrie Swart <johnblackspear@users.noreply.github.com>
    Co-Authored-By: Justin Struk <10167488+JStruk@users.noreply.github.com>

--- src/components/nodes/task/setupMultiInstanceMarkers.js
+++ src/components/nodes/task/setupMultiInstanceMarkers.js
@@ -1,14 +1,27 @@
 import sequentialIcon from '@/assets/sequential.svg';
 import parallelIcon from '@/assets/parallel.svg';
+import loopIcon from '@/assets/loop.svg';
+
+const standardLoop = 'bpmn:StandardLoopCharacteristics';
+const multiInstanceLoop = 'bpmn:MultiInstanceLoopCharacteristics';
 
 export default function setupMultiInstanceMarkers(nodeDefinition, markers, $set) {
   const loopCharacteristics = nodeDefinition.get('loopCharacteristics');
-  const isMultiInstance = loopCharacteristics ?
-    loopCharacteristics.$type === 'bpmn:MultiInstanceLoopCharacteristics' :
-    false;
-  const isSequential = isMultiInstance ? loopCharacteristics.isSequential : false;
 
-  if (isMultiInstance) {
-    $set(markers.bottomCenter, 'multiInstance', isSequential ? sequentialIcon : parallelIcon);
+  const handledLoopTypes = [
+    standardLoop,
+    multiInstanceLoop,
+  ];
+
+  if (!loopCharacteristics || !handledLoopTypes.includes(loopCharacteristics.$type)) {
+    return;
   }
+
+  if (loopCharacteristics.$type === standardLoop) {
+    $set(markers.bottomCenter, 'loopCharacteristics', loopIcon);
+    return;
+  }
+
+  const multiInstanceIcon = loopCharacteristics.isSequential ? sequentialIcon : parallelIcon;
+  $set(markers.bottomCenter, 'loopCharacteristics', multiInstanceIcon);
 }
