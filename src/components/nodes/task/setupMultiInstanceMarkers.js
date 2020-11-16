3c44c51b9fb14d74b400a819c0b9814a2473d418
vehikl-jacare <vehikl-jacare@users.noreply.github.com>
2020-11-16 11:44:42 -0500

    extract setup multi instance task markers method
    Co-Authored-By: Robert Komaromi <chipit24@users.noreply.github.com>
    Co-Authored-By: Gerrie Swart <johnblackspear@users.noreply.github.com>
    Co-Authored-By: Justin Struk <10167488+JStruk@users.noreply.github.com>

---
+++ src/components/nodes/task/setupMultiInstanceMarkers.js
@@ -0,0 +1,15 @@
+import sequentialIcon from '@/assets/sequential.svg';
+import parallelIcon from '@/assets/parallel.svg';
+
+export default function setupMultiInstanceMarker(nodeDefinition, markers, $set) {
+  const loopCharacteristics = nodeDefinition.get('loopCharacteristics');
+  // eslint-disable-next-line no-console
+  console.log(loopCharacteristics);
+  const isMultiInstance = loopCharacteristics ?
+    loopCharacteristics.$type === 'bpmn:MultiInstanceLoopCharacteristics' :
+    false;
+  const isSequential = isMultiInstance ? loopCharacteristics.isSequential : false;
+  if (isMultiInstance) {
+    $set(markers.bottomCenter, 'multiInstance', isSequential ? sequentialIcon : parallelIcon);
+  }
+}
