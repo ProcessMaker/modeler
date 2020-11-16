0707e3d64d122daba2edf4f607555c9a2bc96f1f
vehikl-jacare <vehikl-jacare@users.noreply.github.com>
2020-11-16 14:53:50 -0500

    refactor multi instance marker tests
    Co-Authored-By: Robert Komaromi <chipit24@users.noreply.github.com>
    Co-Authored-By: Gerrie Swart <johnblackspear@users.noreply.github.com>
    Co-Authored-By: Justin Struk <10167488+JStruk@users.noreply.github.com>

--- tests/unit/components/nodes/task/setupMultiInstanceMarkers.spec.js
+++ tests/unit/components/nodes/task/setupMultiInstanceMarkers.spec.js
@@ -1,10 +1,15 @@
 import setupMultiInstanceMarkers from '@/components/nodes/task/setupMultiInstanceMarkers';
 import sequentialIcon from '@/assets/sequential.svg';
 import parallelIcon from '@/assets/parallel.svg';
+import loopIcon from '@/assets/loop.svg';
 
 describe('setupMultiInstanceMarkers', () => {
+  let $set = jest.fn();
+  let markers = {
+    bottomCenter: {},
+  };
+
   it('does not set up markers for task without loop characteristics', () => {
-    const $set = jest.fn();
     const nodeDefinition = { get(){} };
 
     setupMultiInstanceMarkers(nodeDefinition, {}, $set);
@@ -13,12 +18,6 @@ describe('setupMultiInstanceMarkers', () => {
   });
 
   it('sets sequential icon for sequential multi instance task', () => {
-    const $set = jest.fn();
-
-    const markers = {
-      bottomCenter: {},
-    };
-
     const nodeDefinition = {
       loopCharacteristics: {
         $type: 'bpmn:MultiInstanceLoopCharacteristics',
@@ -31,16 +30,10 @@ describe('setupMultiInstanceMarkers', () => {
 
     setupMultiInstanceMarkers(nodeDefinition, markers, $set);
 
-    expect($set).toHaveBeenCalledWith(markers.bottomCenter, 'multiInstance', sequentialIcon);
+    expect($set).toHaveBeenCalledWith(markers.bottomCenter, 'loopCharacteristics', sequentialIcon);
   });
 
   it('sets parallel icon for parallel multi instance task', () => {
-    const $set = jest.fn();
-
-    const markers = {
-      bottomCenter: {},
-    };
-
     const nodeDefinition = {
       loopCharacteristics: {
         $type: 'bpmn:MultiInstanceLoopCharacteristics',
@@ -53,6 +46,36 @@ describe('setupMultiInstanceMarkers', () => {
 
     setupMultiInstanceMarkers(nodeDefinition, markers, $set);
 
-    expect($set).toHaveBeenCalledWith(markers.bottomCenter, 'multiInstance', parallelIcon);
+    expect($set).toHaveBeenCalledWith(markers.bottomCenter, 'loopCharacteristics', parallelIcon);
+  });
+
+  it('sets loop icon for standard loop characteristics task', () => {
+    const nodeDefinition = {
+      loopCharacteristics: {
+        $type: 'bpmn:StandardLoopCharacteristics',
+      },
+      get(prop) {
+        return this[prop];
+      },
+    };
+
+    setupMultiInstanceMarkers(nodeDefinition, markers, $set);
+
+    expect($set).toHaveBeenCalledWith(markers.bottomCenter, 'loopCharacteristics', loopIcon);
+  });
+
+  it('can add a compensation marker', () => {
+    const nodeDefinition = {
+      loopCharacteristics: {
+        $type: 'bpmn:StandardLoopCharacteristics',
+      },
+      get(prop) {
+        return this[prop];
+      },
+    };
+
+    setupMultiInstanceMarkers(nodeDefinition, markers, $set);
+
+    expect($set).toHaveBeenCalledWith(markers.bottomCenter, 'loopCharacteristics', loopIcon);
   });
 });
