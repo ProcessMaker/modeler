e63954e63a188d6e06c87c7c5468fda5eb6ac86d
vehikl-jacare <vehikl-jacare@users.noreply.github.com>
2020-11-20 14:11:36 -0500

    add data association flow button to crown
    Co-Authored-By: Robert Komaromi <chipit24@users.noreply.github.com>
    Co-Authored-By: Gerrie Swart <johnblackspear@users.noreply.github.com>
    Co-Authored-By: Justin Struk <10167488+JStruk@users.noreply.github.com>

---
+++ src/components/crown/crownButtons/dataAssociationFlowButton.vue
@@ -0,0 +1,62 @@
+<template>
+  <crown-button
+    v-if="allowOutgoingDataAssociationFlow"
+    :title="$t('Data Association Flow')"
+    v-b-tooltip.hover.viewport.d50="{ customClass: 'no-pointer-events' }"
+    id="association-flow-button"
+    aria-label="Add association flow"
+    :src="connectIcon"
+    role="menuitem"
+    @click="addAssociation"
+  />
+</template>
+
+<script>
+import connectIcon from '@/assets/connect-artifacts.svg';
+import CrownButton from '@/components/crown/crownButtons/crownButton';
+import { direction } from '@/components/nodes/association/associationConfig';
+import Node from '@/components/nodes/node';
+
+const dataAssociationFlowBlacklist = [
+  'bpmn:EndEvent',
+  'bpmn:MessageFlow',
+  'bpmn:SequenceFlow',
+  'bpmn:Participant',
+  'bpmn:Lane',
+  'bpmn:TextAnnotation',
+  'bpmn:Association',
+  'bpmn:DataObjectReference',
+  'bpmn:DataStoreReference',
+];
+
+export default {
+  components: { CrownButton },
+  props: ['node', 'moddle', 'shape'],
+  data() {
+    return {
+      connectIcon,
+    };
+  },
+  computed: {
+    allowOutgoingDataAssociationFlow() {
+      return !this.node.isBpmnType(...dataAssociationFlowBlacklist);
+    },
+  },
+  methods: {
+    addAssociation(cellView, evt, x, y) {
+      this.$emit('toggle-crown-state', false);
+      const associationLink = this.moddle.create('bpmn:Association', {
+        sourceRef: this.shape.component.node.definition,
+        targetRef: { x, y },
+        associationDirection: direction.none,
+      });
+
+      this.$emit('add-node', new Node(
+        'processmaker-modeler-association',
+        associationLink,
+        this.moddle.create('bpmndi:BPMNEdge'),
+      ));
+    },
+  },
+};
+</script>
