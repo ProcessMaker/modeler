cdaadd252e1dcdf0b9f72042cebc0bbed55b0de1
vehikl-jacare <vehikl-jacare@users.noreply.github.com>
2020-11-30 11:15:47 -0500

    remove compensation icon if isForCompensation is false
    Co-Authored-By: Justin Struk <10167488+JStruk@users.noreply.github.com>
    Co-Authored-By: Robert Komaromi <chipit24@users.noreply.github.com>
    Co-Authored-By: Gerrie Swart <johnblackspear@users.noreply.github.com>

--- src/components/nodes/task/setupCompensationMarker.js
+++ src/components/nodes/task/setupCompensationMarker.js
@@ -3,5 +3,7 @@ import compensationIcon from '@/assets/compensation.svg';
 export default function setupCompensationMarker(nodeDefinition, markers, $set) {
   if (nodeDefinition.isForCompensation) {
     $set(markers.bottomCenter, 'compensation', compensationIcon);
+  } else {
+    $set(markers.bottomCenter, 'compensation', null);
   }
 }
