0d48c7a6c020a81d0975c418b3b7527e79753266
vehikl-jacare <vehikl-jacare@users.noreply.github.com>
2020-12-08 16:39:14 -0500

    extract some utility functions

--- tests/e2e/specs/SubProcess.spec.js
+++ tests/e2e/specs/SubProcess.spec.js
@@ -7,86 +7,89 @@ import {
 
 import { nodeTypes } from '../support/constants';
 
+function selectCalledProcess(calledProcessName) {
+  cy.get('[data-test="inspector-container"]')
+    .contains('Process')
+    .next('.multiselect')
+    .click()
+    .find('.multiselect__content')
+    .contains(calledProcessName)
+    .click();
+}
+
+function selectSubProcessStartEvent(startEventName) {
+  cy.get('[data-test="inspector-container"]')
+    .contains('Start Event')
+    .next('.multiselect')
+    .click()
+    .find('.multiselect__content')
+    .contains(startEventName)
+    .click();
+}
+
+function addSubprocessToPaper(position) {
+  addNodeTypeToPaper(position, nodeTypes.task, 'switch-to-sub-process');
+}
+
+function clickSubprocessPlusButton() {
+  cy.get('.main-paper [data-type="processmaker.components.nodes.task.Shape"] [joint-selector="bottomCenter.0"]').click();
+}
+
 describe('Sub Process (Call Activities)', () => {
-  const taskPosition = { x: 250, y: 250 };
+  const subProcessPosition = { x: 250, y: 250 };
   const testString = 'testing';
+  const defaultSubProcessNodeName = 'Sub Process';
 
   it('Can create sub process with config', () => {
-    const processName = 'Process with multiple start events';
-    const subProcessPosition = { x: 250, y: 250 };
-    const subProcessName = 'Sub Process';
+    const calledProcessName = 'Process with multiple start events';
     const startEventName = 'Start Event Two';
     const encodedConfig = JSON.stringify({
       calledElement: 'Subprocess1-5',
       processId: 5,
       startEvent: 'node_10',
-      name: `${subProcessName}`,
+      name: `${defaultSubProcessNodeName}`,
     }).replace(/"/g, '&#34;');
 
-    addNodeTypeToPaper(subProcessPosition, nodeTypes.task, 'switch-to-sub-process');
-    getElementAtPosition(subProcessPosition)
-      .click({ force: true });
+    addSubprocessToPaper(subProcessPosition);
+    getElementAtPosition(subProcessPosition).click({ force: true });
 
-    cy.get('[data-test="inspector-container"]')
-      .contains('Process')
-      .next('.multiselect')
-      .click()
-      .find('.multiselect__content')
-      .contains(processName)
-      .click();
+    selectCalledProcess(calledProcessName);
+    selectSubProcessStartEvent(startEventName);
 
-    cy.get('[data-test="inspector-container"]')
-      .contains('Start Event')
-      .next('.multiselect')
-      .click()
-      .find('.multiselect__content')
-      .contains(startEventName)
-      .click();
-
-    assertDownloadedXmlContainsExpected(`<bpmn:callActivity id="node_3" name="${subProcessName}" calledElement="Subprocess1-5" pm:config="${encodedConfig}" />`);
+    assertDownloadedXmlContainsExpected(`<bpmn:callActivity id="node_3" name="${defaultSubProcessNodeName}" calledElement="Subprocess1-5" pm:config="${encodedConfig}" />`);
   });
 
   it('Allows typing in name field on new sub process', () => {
-    addNodeTypeToPaper(taskPosition, nodeTypes.task, 'switch-to-sub-process');
+    addSubprocessToPaper(subProcessPosition);
     typeIntoTextInput('[name=name]', testString);
     cy.get('[name=name]').should('have.value', testString);
   });
 
   it('shows a message when clicking the [+] if no subprocess selected', () => {
-    const subProcessPosition = { x: 250, y: 250 };
+    addSubprocessToPaper(subProcessPosition);
 
-    addNodeTypeToPaper(subProcessPosition, nodeTypes.task, 'switch-to-sub-process');
+    clickSubprocessPlusButton();
 
-    cy.get('.main-paper [data-type="processmaker.components.nodes.task.Shape"] [joint-selector="bottomCenter.0"]').click();
     cy.get('.modal-content')
       .should('contain.text', 'No subprocess selected')
       .should('contain.text', 'Please select a subprocess to view it.');
   });
 
   it('shows a subprocess SVG modal when clicking the [+] button', () => {
-    const processName = 'Process with multiple start events';
-    const subProcessPosition = { x: 250, y: 250 };
-    const subProcessName = 'Sub Process';
-    const subProcessId = 5;
+    const calledProcessName = 'Process with multiple start events';
+    const calledSubProcessId = 5;
 
-    addNodeTypeToPaper(subProcessPosition, nodeTypes.task, 'switch-to-sub-process');
-    getElementAtPosition(subProcessPosition)
-      .click({ force: true });
+    addSubprocessToPaper(subProcessPosition);
+    getElementAtPosition(subProcessPosition).click({ force: true });
 
-    cy.get('[data-test="inspector-container"]')
-      .contains('Process')
-      .next('.multiselect')
-      .click()
-      .find('.multiselect__content')
-      .contains(processName)
-      .click();
+    selectCalledProcess(calledProcessName);
 
     cy.clock();
-    cy.get('.main-paper [data-type="processmaker.components.nodes.task.Shape"] [joint-selector="bottomCenter.0"]').click();
+    clickSubprocessPlusButton();
 
     cy.tick(500);
     cy.get('.modal-content')
-      .should('contain.text', `You are viewing subprocess for '${subProcessName}'`)
+      .should('contain.text', `You are viewing subprocess for '${defaultSubProcessNodeName}'`)
       .should('contain.text', 'Loading process preview...');
 
     cy.tick(1000);
@@ -94,27 +97,19 @@ describe('Sub Process (Call Activities)', () => {
       .should('not.contain.text', 'Loading process preview...')
       .should('contain.html', '<svg');
 
-    cy.get('[data-test="modal-process-link"]').should('have.attr', 'href', `/modeler/${subProcessId}`);
+    cy.get('[data-test="modal-process-link"]').should('have.attr', 'href', `/modeler/${calledSubProcessId}`);
   });
 
-  it.only('shows error message when loading of SVG fails', () => {
-    const processName = 'Process with mocked axios failure';
-    const subProcessPosition = { x: 250, y: 250 };
+  it('shows error message when loading of SVG fails', () => {
+    const calledProcessName = 'Process with mocked axios failure';
 
-    addNodeTypeToPaper(subProcessPosition, nodeTypes.task, 'switch-to-sub-process');
-    getElementAtPosition(subProcessPosition)
-      .click({ force: true });
+    addSubprocessToPaper(subProcessPosition);
+    getElementAtPosition(subProcessPosition).click({ force: true });
 
-    cy.get('[data-test="inspector-container"]')
-      .contains('Process')
-      .next('.multiselect')
-      .click()
-      .find('.multiselect__content')
-      .contains(processName)
-      .click();
+    selectCalledProcess(calledProcessName);
 
     cy.clock();
-    cy.get('.main-paper [data-type="processmaker.components.nodes.task.Shape"] [joint-selector="bottomCenter.0"]').click();
+    clickSubprocessPlusButton();
     cy.tick(2000);
     cy.get('.modal-content').should('contain.text', 'Could not load preview');
   });
