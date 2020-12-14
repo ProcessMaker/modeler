2c80a75c3b4bda87d88ff75c55ecd978b39ec03c
vehikl-jacare <vehikl-jacare@users.noreply.github.com>
2020-12-14 10:34:09 -0500

    perform node replacement in atomic action
    Co-Authored-By: Justin Struk <10167488+JStruk@users.noreply.github.com>
    Co-Authored-By: Robert Komaromi <chipit24@users.noreply.github.com>
    Co-Authored-By: Gerrie Swart <johnblackspear@users.noreply.github.com>

--- tests/e2e/specs/SwitchingElements.spec.js
+++ tests/e2e/specs/SwitchingElements.spec.js
@@ -1,9 +1,10 @@
 import {
+  assertDownloadedXmlContainsExpected,
   assertDownloadedXmlDoesNotContainExpected,
   connectNodesWithFlow,
   dragFromSourceToDest,
   getElementAtPosition,
-  getLinksConnectedToElement, modalConfirm,
+  getLinksConnectedToElement, modalConfirm, waitToRenderAllShapes,
 } from '../support/utils';
 import {nodeTypes} from '../support/constants';
 
@@ -41,5 +42,26 @@ describe('Switching elements', () => {
     changeGatewayTypeTo('parallel-gateway', gatewayPosition);
 
     assertDownloadedXmlDoesNotContainExpected(flowExpression);
+  });
+
+  it('add a single undo state when replacing a node in place', () => {
+    const startEventPosition = { x: 150, y: 150 };
+    const initialStartEvent = '<bpmn:startEvent id="node_1" name="Start Event" />';
+    const replacementStartEvent = '<bpmn:startEvent id="node_2" name="Message Start Event">';
+    assertDownloadedXmlContainsExpected(initialStartEvent);
+
+    getElementAtPosition(startEventPosition).click();
+    cy.get('[data-test=select-type-dropdown]').click();
+    cy.get('[data-test=switch-to-message-start-event]').click();
+    modalConfirm();
+    // if we see two nodes here the replacement failed completely
+    assertDownloadedXmlContainsExpected(replacementStartEvent);
+    assertDownloadedXmlDoesNotContainExpected(initialStartEvent);
+
+    cy.get('[data-test=undo]').click();
+    waitToRenderAllShapes();
+    // if we see two nodes here then we had an intermediate invalid undo state saved
+    assertDownloadedXmlContainsExpected(initialStartEvent);
+    assertDownloadedXmlDoesNotContainExpected(replacementStartEvent);
   });
 });
