04d16dcba201957b0c126b16d18c980a05897970
vehikl-jacare <vehikl-jacare@users.noreply.github.com>
2020-12-17 15:13:37 -0500

    set default icon names to empty string
    Co-Authored-By: Justin Struk <10167488+JStruk@users.noreply.github.com>
    Co-Authored-By: Robert Komaromi <chipit24@users.noreply.github.com>
    Co-Authored-By: Gerrie Swart <johnblackspear@users.noreply.github.com>

--- src/components/nodes/task/task.vue
+++ src/components/nodes/task/task.vue
@@ -85,7 +85,7 @@ export default {
       ],
       boundaryEventDropdownData,
       anchorPointFunction: getRectangleAnchorPoint,
-      iconName: 'user',
+      iconName: '',
     };
   },
   computed: {
