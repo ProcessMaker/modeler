3be7a29bdf1428391be596b4be37970d78bb9c4b
vehikl-jacare <68430564+vehikl-jacare@users.noreply.github.com>
2021-01-12 14:49:01 -0500

    Save pools with data objects and stores (#1325)
    Co-Authored-By: Robert Komaromi <chipit24@users.noreply.github.com>
    Co-Authored-By: Gerrie Swart <johnblackspear@users.noreply.github.com>
    Co-Authored-By: Justin Struk <JStruk@users.noreply.github.com>

--- tests/e2e/specs/Pools.spec.js
+++ tests/e2e/specs/Pools.spec.js
@@ -10,19 +10,19 @@ import {
   isElementCovered,
   moveElement,
   moveElementRelativeTo,
-  removeIndentationAndLinebreaks,
+  removeIndentationAndLinebreaks, removeStartEvent,
   setBoundaryEvent,
   typeIntoTextInput,
   waitToRenderAllShapes,
 } from '../support/utils';
 
-import { nodeTypes } from '../support/constants';
+import {nodeTypes} from '../support/constants';
 
 describe('Pools', () => {
   it('Update pool name', () => {
     const testString = 'testing';
 
-    const poolPosition = { x: 200, y: 200 };
+    const poolPosition = {x: 200, y: 200};
     dragFromSourceToDest(nodeTypes.pool, poolPosition);
 
     getElementAtPosition(poolPosition).click();
@@ -32,30 +32,30 @@ describe('Pools', () => {
   });
 
   it('Can add top and bottom lane', () => {
-    const poolPosition = { x: 300, y: 300 };
+    const poolPosition = {x: 300, y: 300};
 
     dragFromSourceToDest(nodeTypes.pool, poolPosition);
 
     getElementAtPosition(poolPosition)
       .click()
       .then($pool => {
-        getCrownButtonForElement($pool, 'lane-above-button').click({ force: true });
-        getCrownButtonForElement($pool, 'lane-below-button').click({ force: true });
+        getCrownButtonForElement($pool, 'lane-above-button').click({force: true});
+        getCrownButtonForElement($pool, 'lane-below-button').click({force: true});
       });
   });
 
   it('Does not allow pool with only one lane', () => {
-    const poolPosition = { x: 300, y: 300 };
-    const lanePosition = { x: 300, y: 550 };
+    const poolPosition = {x: 300, y: 300};
+    const lanePosition = {x: 300, y: 550};
 
     dragFromSourceToDest(nodeTypes.pool, poolPosition);
 
     getElementAtPosition(poolPosition).click().then($pool => {
-      getCrownButtonForElement($pool, 'lane-below-button').click({ force: true });
+      getCrownButtonForElement($pool, 'lane-below-button').click({force: true});
     });
 
     getElementAtPosition(poolPosition).click().then($pool => {
-      getCrownButtonForElement($pool, 'delete-button').click({ force: true });
+      getCrownButtonForElement($pool, 'delete-button').click({force: true});
     });
 
     getElementAtPosition(lanePosition).getType().should('not.equal', nodeTypes.lane);
@@ -63,9 +63,9 @@ describe('Pools', () => {
   });
 
   it('Can drag elements between pools', () => {
-    const startEventPosition = { x: 150, y: 150 };
+    const startEventPosition = {x: 150, y: 150};
 
-    const pool1Position = { x: 200, y: 200 };
+    const pool1Position = {x: 200, y: 200};
     dragFromSourceToDest(nodeTypes.pool, pool1Position);
 
     const startEventIn1stPool = '<bpmn:process id="Process_1" isExecutable="true"><bpmn:startEvent id="node_1" name="Start Event" /></bpmn:process>';
@@ -78,7 +78,7 @@ describe('Pools', () => {
         expect(xml).to.contain(startEventIn1stPool);
       });
 
-    const pool2Position = { x: 200, y: 500 };
+    const pool2Position = {x: 200, y: 500};
     dragFromSourceToDest(nodeTypes.pool, pool2Position);
 
     moveElement(startEventPosition, pool2Position);
@@ -112,19 +112,19 @@ describe('Pools', () => {
   });
 
   it('remove all references of flows when deleting a pool with a process', () => {
-    const startEventPosition = { x: 150, y: 150 };
-    const taskPosition = { x: 350, y: 350 };
+    const startEventPosition = {x: 150, y: 150};
+    const taskPosition = {x: 350, y: 350};
 
     dragFromSourceToDest(nodeTypes.task, taskPosition);
 
-    const poolPosition = { x: 300, y: 300 };
+    const poolPosition = {x: 300, y: 300};
     dragFromSourceToDest(nodeTypes.pool, poolPosition);
     connectNodesWithFlow('sequence-flow-button', startEventPosition, taskPosition);
 
     getElementAtPosition(poolPosition)
       .click()
       .then($pool => {
-        getCrownButtonForElement($pool, 'delete-button').click({ force: true });
+        getCrownButtonForElement($pool, 'delete-button').click({force: true});
       });
 
     const sequenceFlowReference = '<bpmn:sequenceFlow id="node_3" name="Sequence Flow" sourceRef="node_1" targetRef="node_2" />';
@@ -139,19 +139,19 @@ describe('Pools', () => {
   });
 
   it('Removes all references to element from lane', () => {
-    const poolPosition = { x: 300, y: 300 };
+    const poolPosition = {x: 300, y: 300};
     dragFromSourceToDest(nodeTypes.pool, poolPosition);
 
     getElementAtPosition(poolPosition)
       .click()
       .then($pool => {
-        getCrownButtonForElement($pool, 'lane-below-button').click({ force: true });
+        getCrownButtonForElement($pool, 'lane-below-button').click({force: true});
       });
 
     const nonEmptyLane = '<bpmn:lane id="node_4" name=""><bpmn:flowNodeRef>node_1</bpmn:flowNodeRef></bpmn:lane>';
     assertDownloadedXmlContainsExpected(nonEmptyLane);
 
-    const startEventPosition = { x: 150, y: 150 };
+    const startEventPosition = {x: 150, y: 150};
     getElementAtPosition(startEventPosition)
       .click()
       .then($startEvent => {
@@ -164,10 +164,10 @@ describe('Pools', () => {
   });
 
   it('Removes all references to element from a pool', () => {
-    const poolPosition = { x: 300, y: 300 };
+    const poolPosition = {x: 300, y: 300};
     dragFromSourceToDest(nodeTypes.pool, poolPosition);
 
-    const startEventPosition = { x: 150, y: 150 };
+    const startEventPosition = {x: 150, y: 150};
     getElementAtPosition(startEventPosition).click().then($startEvent => {
       getCrownButtonForElement($startEvent, 'delete-button').click();
     });
@@ -182,16 +182,16 @@ describe('Pools', () => {
   });
 
   it('moves boundary event to another process when dragged to that pool', () => {
-    const poolPosition = { x: 300, y: 300 };
+    const poolPosition = {x: 300, y: 300};
     dragFromSourceToDest(nodeTypes.pool, poolPosition);
 
-    const pool2Position = { x: 100, y: 500 };
+    const pool2Position = {x: 100, y: 500};
     dragFromSourceToDest(nodeTypes.pool, pool2Position);
 
-    const taskPosition = { x: 200, y: 200 };
+    const taskPosition = {x: 200, y: 200};
     dragFromSourceToDest(nodeTypes.task, taskPosition);
 
-    const boundaryTimerEventPosition = { x: 260, y: 260 };
+    const boundaryTimerEventPosition = {x: 260, y: 260};
     setBoundaryEvent(nodeTypes.boundaryTimerEvent, taskPosition);
     moveElement(taskPosition, boundaryTimerEventPosition.x, boundaryTimerEventPosition.y);
 
@@ -214,10 +214,10 @@ describe('Pools', () => {
   });
 
   it('should revert pool element to initial position on undo after dragging outside of pool onto grid', () => {
-    const poolPosition = { x: 300, y: 300 };
+    const poolPosition = {x: 300, y: 300};
     dragFromSourceToDest(nodeTypes.pool, poolPosition);
 
-    const startEventPosition = { x: 150, y: 150 };
+    const startEventPosition = {x: 150, y: 150};
     moveElementRelativeTo(startEventPosition, 600, 600, nodeTypes.startEvent);
 
     waitToRenderAllShapes();
@@ -228,23 +228,23 @@ describe('Pools', () => {
   });
 
   it('should not cover child elements with lane', () => {
-    const poolPosition = { x: 300, y: 300 };
+    const poolPosition = {x: 300, y: 300};
     dragFromSourceToDest(nodeTypes.pool, poolPosition);
 
     getElementAtPosition(poolPosition, nodeTypes.pool)
       .click()
       .then($pool => {
-        getCrownButtonForElement($pool, 'lane-above-button').click({ force: true });
+        getCrownButtonForElement($pool, 'lane-above-button').click({force: true});
       });
 
-    const startEventPosition = { x: 150, y: 150 };
-    const topLanePosition = { x: startEventPosition.x, y: startEventPosition.y - 50 };
-    getPositionInPaperCoords(startEventPosition).then(({ x, y }) => {
+    const startEventPosition = {x: 150, y: 150};
+    const topLanePosition = {x: startEventPosition.x, y: startEventPosition.y - 50};
+    getPositionInPaperCoords(startEventPosition).then(({x, y}) => {
       getElementAtPosition(topLanePosition, nodeTypes.poolLane).click();
 
       cy.get('.main-paper [button-id="bottom-right-resize-button"]')
         .trigger('mousedown')
-        .trigger('mousemove', { clientX: x, clientY: y + 100, force: true })
+        .trigger('mousemove', {clientX: x, clientY: y + 100, force: true})
         .trigger('mouseup');
     });
 
@@ -253,40 +253,40 @@ describe('Pools', () => {
   });
 
   it('does not move boundary events independently from tasks when moving pool', () => {
-    const taskPosition = { x: 200, y: 200 };
+    const taskPosition = {x: 200, y: 200};
     dragFromSourceToDest(nodeTypes.task, taskPosition);
     setBoundaryEvent(nodeTypes.boundaryTimerEvent, taskPosition);
 
-    const poolPosition = { x: 300, y: 300 };
+    const poolPosition = {x: 300, y: 300};
     dragFromSourceToDest(nodeTypes.pool, poolPosition);
 
     getElementAtPosition(poolPosition).then($pool => {
       assertBoundaryEventIsCloseToTask();
 
       cy.wrap($pool)
-        .trigger('mousedown', { which: 1, force: true })
-        .trigger('mousemove', { clientX: 800, clientY: 350, force: true })
-        .trigger('mouseup', { force: true })
+        .trigger('mousedown', {which: 1, force: true})
+        .trigger('mousemove', {clientX: 800, clientY: 350, force: true})
+        .trigger('mouseup', {force: true})
         .then(waitToRenderAllShapes)
         .then(assertBoundaryEventIsCloseToTask);
     });
   });
 
   it('Check LaneSet IDs', () => {
-    const poolPosition1 = { x: 300, y: 150 };
-    const poolPosition2 = { x: 300, y: 400 };
+    const poolPosition1 = {x: 300, y: 150};
+    const poolPosition2 = {x: 300, y: 400};
 
     // Add pool
     dragFromSourceToDest(nodeTypes.pool, poolPosition1);
 
     getElementAtPosition(poolPosition1).click().then($pool => {
-      getCrownButtonForElement($pool, 'lane-below-button').click({ force: true });
+      getCrownButtonForElement($pool, 'lane-below-button').click({force: true});
     });
 
     dragFromSourceToDest(nodeTypes.pool, poolPosition2);
 
-    getElementAtPosition({ x: 600, y: 600 }).click().then($pool => {
-      getCrownButtonForElement($pool, 'lane-below-button').click({ force: true });
+    getElementAtPosition({x: 600, y: 600}).click().then($pool => {
+      getCrownButtonForElement($pool, 'lane-below-button').click({force: true});
     });
 
     const laneSet1IdBpmn = `
@@ -305,5 +305,45 @@ describe('Pools', () => {
     `;
     assertDownloadedXmlContainsExpected(laneSet1IdBpmn);
     assertDownloadedXmlContainsExpected(laneSet2IdBpmn);
+  });
+
+  describe('does not add flow node references to pool lanes for excluded items', () => {
+
+    const poolPosition = {x: 100, y: 50};
+
+    function addLaneBelow() {
+      getElementAtPosition({x: poolPosition.x + 100, y: poolPosition.y + 100}, nodeTypes.pool)
+        .click({force: true})
+        .then($pool => {
+          getCrownButtonForElement($pool, 'lane-below-button').click({force: true});
+        });
+    }
+
+    function addElementsThatShouldNotHaveFlowNodeRefs() {
+      dragFromSourceToDest(nodeTypes.dataStore, {x: poolPosition.x + 100, y: poolPosition.y + 100});
+      dragFromSourceToDest(nodeTypes.dataObject, {x: poolPosition.x + 150, y: poolPosition.y + 150});
+      dragFromSourceToDest(nodeTypes.textAnnotation, {x: poolPosition.x + 200, y: poolPosition.y + 200});
+    }
+
+    it('when adding elements to an existing lane', () => {
+      removeStartEvent();
+      dragFromSourceToDest(nodeTypes.pool, poolPosition);
+
+      addLaneBelow();
+      addElementsThatShouldNotHaveFlowNodeRefs();
+
+      assertDownloadedXmlDoesNotContainExpected('<bpmn:flowNodeRef');
+    });
+
+    it('when adding elements to a pool and then adding a lane', () => {
+      removeStartEvent();
+      dragFromSourceToDest(nodeTypes.pool, poolPosition);
+
+      addElementsThatShouldNotHaveFlowNodeRefs();
+      addLaneBelow();
+
+      assertDownloadedXmlDoesNotContainExpected('<bpmn:flowNodeRef');
+    });
+
   });
 });
