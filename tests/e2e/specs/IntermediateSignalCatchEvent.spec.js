73c82bb9913129fe6fa38534ab8d4da5c61c3af6
David Callizaya <davidcallizaya@gmail.com>
2021-08-02 12:10:01 -0400

    Test request variable in  signal catch events

--- tests/e2e/specs/IntermediateSignalCatchEvent.spec.js
+++ tests/e2e/specs/IntermediateSignalCatchEvent.spec.js
@@ -12,4 +12,18 @@ describe('Intermediate Signal Catch Event', () => {
       </bpmn:intermediateCatchEvent>
     `);
   });
+
+  it('Configure the signal request variable', () => {
+    const intermediateSignalCatchEventPosition = { x: 250, y: 250 };
+    addNodeTypeToPaper(intermediateSignalCatchEventPosition, nodeTypes.intermediateCatchEvent, 'switch-to-intermediate-signal-catch-event');
+
+    // Config the signal request variable in the inspector
+    cy.get('input[data-cy="signal-request-variable"]').type('signalRequestVariable');
+
+    assertDownloadedXmlContainsExpected(`
+      <bpmn:intermediateCatchEvent id="node_3" name="Intermediate Signal Catch Event" pm:config="signalRequestVariable">
+        <bpmn:signalEventDefinition />
+      </bpmn:intermediateCatchEvent>
+    `);
+  });
 });
