031106afd6b97f9c9dbd6d3b4c2ad7fd81e513ea
David Callizaya <davidcallizaya@gmail.com>
2021-08-17 16:36:51 -0400

    Delete a Pool, should remove event messages too

--- tests/e2e/specs/PoolsAdvanced.spec.js
+++ tests/e2e/specs/PoolsAdvanced.spec.js
@@ -31,4 +31,17 @@ describe('Pools', () => {
       assertDownloadedXmlContainsExpected(atob(data));
     });
   });
+
+  it('Case 3 Delete a Pool, should remove event messages too', () => {
+    uploadXml('Pools-FlowMessageBetweenPools.xml');
+    waitToRenderAllShapes();
+    cy.get('g[data-type="processmaker.modeler.bpmn.pool"]:visible').eq(1)
+      .click({ force: true })
+      .then($pool => {
+        getCrownButtonForElement($pool, 'delete-button').click({ force: true });
+      });
+    cy.fixture('Pools-FlowMessageBetweenPools.after.xml', 'base64').then(data => {
+      assertDownloadedXmlContainsExpected(atob(data));
+    });
+  });
 });
