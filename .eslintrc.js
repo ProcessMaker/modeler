d73b721649474b2ab047c250e127ba4843228c9b
Teisha McRae <mcraeteisha@gmail.com>
2022-08-05 16:07:27 -0400

    Install eslint with configs for prettier and airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,22 @@
 module.exports = {
-  root: true,
-
   env: {
-    node: true,
+    browser: true,
+    es2021: true,
   },
-
   extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
+    'plugin:vue/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended'
   ],
-
-  rules: {
-    semi: ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    indent: ['error', 2, { SwitchCase: 1 }],
-    quotes: ['error', 'single'],
-    'object-curly-spacing': ['warn', 'always', { 'arraysInObjects': false }],
-    'no-debugger': 'off',
-    'object-shorthand': 'error',
-    'space-before-function-paren': ['error', 'never'],
-    'keyword-spacing': 'error',
-    'vue/html-indent': ['error', 2, { alignAttributesVertically: false }],
-    'vue/html-closing-bracket-newline': ['error', {
-      singleline: 'never',
-      multiline: 'always',
-    }],
-    'vue/html-self-closing': 'error',
-    'vue/mustache-interpolation-spacing': 'error',
-    'template-curly-spacing': 'error',
-  },
-
   parserOptions: {
-    parser: 'babel-eslint',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error'
   },
 };
