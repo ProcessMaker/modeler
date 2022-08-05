e67fb956f702c0d942b7167cdfa461683cb6a268
Teisha McRae <mcraeteisha@gmail.com>
2022-08-05 16:28:44 -0400

    Updated dependency versions for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,48 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2020: true,
+    node: true
   },
+
   extends: [
     'plugin:vue/recommended',
     'airbnb-base',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    '@vue/prettier',
+    'plugin:vue/essential',
+    '@vue/airbnb'
   ],
+
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    parser: 'babel-eslint'
   },
+
   plugins: [
     'vue',
     'prettier'
   ],
+
   rules: {
-    'prettier/prettier': 'error'
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
+
+  root: true,
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    },
+  ],
 };
