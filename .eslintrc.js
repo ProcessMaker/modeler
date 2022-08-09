0a18c90a8d48a1a425296c4c5c4b55b07cad77d6
Jose Chirivella <jose.chirivella@processmaker.com>
2022-08-09 16:47:50 -0400

    FOUR-6064 EcmaVersion set to 6 since it solves an issue and added double quotes as the default

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,47 +2,42 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
-    node: true
+    node: true,
   },
 
   extends: [
-    'plugin:vue/recommended',
-    'airbnb-base',
-    'plugin:prettier/recommended',
-    'eslint:recommended',
-    '@vue/prettier',
-    'plugin:vue/essential',
-    '@vue/airbnb'
+    "plugin:vue/recommended",
+    "airbnb-base",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "@vue/prettier",
+    "plugin:vue/essential",
+    "@vue/airbnb",
   ],
 
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    parser: 'babel-eslint'
+    ecmaVersion: "6",
+    sourceType: "module",
+    parser: "babel-eslint",
   },
 
-  plugins: [
-    'vue',
-    'prettier'
-  ],
+  plugins: ["vue", "prettier"],
 
   rules: {
-    'prettier/prettier': 'error',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "prettier/prettier": "error",
+    quotes: ["error", "double"],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 
   root: true,
 
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
       env: {
-        jest: true
-      }
+        jest: true,
+      },
     },
   ],
 };
