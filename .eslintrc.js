b86d5b80c494947eef90d6d22aa9d5e562e058bd
Jose Chirivella <jose.chirivella@processmaker.com>
2022-08-11 21:28:48 -0400

    FOUR-6064 Eslint and Prettier changes * Added more rules and modified the existing ones

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,29 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
-    node: true,
+    node: true
   },
 
-  extends: [
-    "plugin:vue/recommended",
-    "airbnb-base",
-    "plugin:prettier/recommended",
-    "eslint:recommended",
-    "@vue/prettier",
-    "plugin:vue/essential",
-    "@vue/airbnb",
-  ],
+  extends: ["plugin:vue/recommended", "airbnb-base", "eslint:recommended", "@vue/prettier", "@vue/airbnb", "plugin:prettier/recommended"],
 
   parserOptions: {
     ecmaVersion: "6",
     sourceType: "module",
-    parser: "babel-eslint",
+    parser: "babel-eslint"
   },
 
   plugins: ["vue", "prettier"],
 
   rules: {
-    "prettier/prettier": "error",
-    quotes: ["error", "double"],
+    "prettier/prettier": ["error", { trailingComma: "none" }],
+    "no-unexpected-multiline": "error",
+    "no-param-reassign": 1,
+    eqeqeq: "error",
+    "max-len": ["error", { code: 140, ignoreUrls: true }],
+    "comma-dangle": ["error", "never"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   },
 
   root: true,
@@ -36,8 +33,8 @@ module.exports = {
     {
       files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
       env: {
-        jest: true,
-      },
-    },
-  ],
+        jest: true
+      }
+    }
+  ]
 };
