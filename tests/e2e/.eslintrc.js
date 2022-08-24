7170ea79e2b01180bc6cffbfcf13314e69de30a5
Jose Chirivella <jose.chirivella@processmaker.com>
2022-08-24 12:35:03 -0400

    FOUR-6081 New cypress eslint rules

--- tests/e2e/.eslintrc.js
+++ tests/e2e/.eslintrc.js
@@ -1,25 +1,23 @@
 module.exports = {
-  plugins: ["cypress"],
+  plugins: ["vue", "prettier", "cypress"],
   env: {
     mocha: true,
     "cypress/globals": true,
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended"],
+  extends: ["plugin:vue/recommended", "airbnb-base", "eslint:recommended", "@vue/prettier", "@vue/airbnb", "plugin:prettier/recommended"],
   rules: {
-    strict: "off",
-    semi: ["error", "always"],
     "comma-dangle": ["error", "only-multiline"],
     indent: ["error", 2, { SwitchCase: 1 }],
     "vue/html-indent": "error",
-    quotes: ["error", "single"],
     "no-debugger": "off",
     "object-shorthand": "error",
     "space-before-function-paren": ["error", "never"],
-    "keyword-spacing": "error",
-    "prefer-arrow-callback": "error"
+    "keyword-spacing": "error"
   },
   parserOptions: {
+    ecmaVersion: "6",
+    sourceType: "module",
     parser: "babel-eslint"
   }
 };
