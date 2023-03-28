b749d5868ded8e2e95982555b931d61142aea13e
David Callizaya <davidcallizaya@gmail.com>
2023-03-28 19:08:30 -0400

    Fix selection of multiple nodes

--- src/components/nodes/pool/poolEventHandlers.js
+++ src/components/nodes/pool/poolEventHandlers.js
@@ -25,7 +25,7 @@ export default class PoolEventHandlers {
 
   onPointerDown(cellView) {
     if (this.isNotPoolChild(cellView.model)) {
-      this.component.$emit('clearSelection');
+      this.component.$emit('select-pool');
       return;
     }
 
