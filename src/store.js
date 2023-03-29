40f7733e9545da68cbc25cbf8d20a94351dd22af
David Callizaya <davidcallizaya@gmail.com>
2023-03-29 06:58:31 -0400

    Fix highlight nodes

--- src/store.js
+++ src/store.js
@@ -100,7 +100,12 @@ export default new Vuex.Store({
         .map(node => node.id);
       state.highlightedNodes = highlightedNodes
         // remove from selection the selected child nodes in the pool
-        .filter(node => node.pool?.component?.node?.id && !selectedPoolsIds.includes(node.pool.component.node.id));
+        .filter(node => {
+          if (node.pool?.component?.node?.id) {
+            return !selectedPoolsIds.includes(node.pool.component.node.id);
+          }
+          return true;
+        });
     },
     addNode(state, node) {
       /* Add an unchanging ID that Vue can use to track the component
