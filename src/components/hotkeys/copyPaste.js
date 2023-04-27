b424683c5ac8063391562083da6779418977148e
Rodrigo Quelca <rodrigo@processmaker.com>
2023-04-27 14:53:49 -0400

    FOUR-8301: The mouse does double focus in modeler

--- src/components/hotkeys/copyPaste.js
+++ src/components/hotkeys/copyPaste.js
@@ -1,17 +1,19 @@
 export default {
   methods: {
     copyPasteHandler(event, options) {
-      const node = event.target.nodeName.toLowerCase();
-      const isBody = node === 'body';
-      const key = event.key.toLowerCase();
-      const isCopy = key === 'c';
-      const isPaste = key === 'v';
-
-      if (isBody && isCopy && options.mod) {
-        this.copy(event);
-      }
-      if (isBody && isPaste && options.mod) {
-        this.paste(event);
+      const selectedText = window.getSelection().toString();
+      if (!selectedText) {
+        const node = event.target.nodeName.toLowerCase();
+        const isBody = node === 'body';
+        const key = event.key.toLowerCase();
+        const isCopy = key === 'c';
+        const isPaste = key === 'v';
+        if (isBody && isCopy && options.mod) {
+          this.copy(event);
+        }
+        if (isBody && isPaste && options.mod) {
+          this.paste(event);
+        }
       }
     },
     copy(event) {
