5f0c13045d68244bd04a5b9cb3da3b96c7e25763
Miguel Angel <miguelangeldev@icloud.com>
2023-05-05 12:53:06 -0400

    feat: add new bottom rail layout

---
+++ src/components/railBottom/undoRedoControl/UndoRedoControl.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ur-box">
+    <button type="button"
+      class="ur-button"
+      data-test="undo"
+    >
+      <img :src="undoIcon" alt="Undo" >
+    </button>
+
+    <div class="ur-divider" />
+
+    <button type="button"
+      class="ur-button"
+      data-test="redo"
+    >
+      <img :src="redoIcon" alt="Redo" >
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default ({
+  data() {
+    return {
+      undoIcon: require('@/assets/railBottom/undo.svg'),
+      redoIcon: require('@/assets/railBottom/redo.svg'),
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped src="./undoRedoControl.scss"></style>
