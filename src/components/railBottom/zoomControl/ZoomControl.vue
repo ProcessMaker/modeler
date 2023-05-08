f1c9810b147147a5ca4161ba303eb9fa1f430709
Miguel Angel <miguelangeldev@icloud.com>
2023-05-08 15:45:27 -0400

    feat: add functionality to zoom controls

---
+++ src/components/railBottom/zoomControl/ZoomControl.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="zoom-box">
+    <button
+      type="button"
+      class="zoom-button"
+      data-test="zoom-out"
+      @click="paperManager.scale = Math.max(minimumScale, paperManager.scale.sx -= scaleStep)"
+      v-b-tooltip.hover
+      :title="$t('Zoom Out')"
+    >
+      <MinusIcon />
+    </button>
+
+    <div v-if="paperManager" class="zoom-text">
+      {{ Math.round(paperManager.scale.sx*100) }}%
+    </div>
+
+    <button
+      type="button"
+      class="zoom-button"
+      data-test="zoom-in"
+      @click="paperManager.scale = paperManager.scale.sx + scaleStep"
+      v-b-tooltip.hover
+      :title="$t('Zoom In')"
+    >
+      <PlusIcon />
+    </button>
+  </div>
+</template>
+
+<script>
+import { MinusIcon, PlusIcon } from '@/components/railBottom/icons';
+
+export default ({
+  components: {
+    MinusIcon,
+    PlusIcon,
+  },
+  props: {
+    paperManager: {},
+  },
+  data() {
+    return {
+      initialScale: 1,
+      minimumScale: 0.2,
+      scaleStep: 0.1,
+    };
+  },
+});
+
+</script>
+
+<style lang="scss" scoped src="./zoomControl.scss"></style>
