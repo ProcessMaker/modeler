6fad49d39cd9c6161beab0f140931d4ead5fa712
Rodrigo Quelca <rodrigo@processmaker.com>
2023-05-09 08:59:10 -0400

    add start event submenu

--- src/components/railBottom/controls/Controls.vue
+++ src/components/railBottom/controls/Controls.vue
@@ -3,37 +3,7 @@
     <ul class="control-list">
 
       <li v-for="(item, key) in controlList" class="control-item" :id="item.id" :key="key">
-        <!-- <div class="control-item-options">
-          <span />
-        </div>
-        <img :src=item.iconSrc :alt=item.label> -->
-
-        <popper
-          trigger="clickToOpen"
-          :options="{
-            placement: 'top',
-            modifiers: { offset: { offset: '0,10px' } }
-          }"
-          :visible-arrow=false
-        >
-          <div class="popper">
-            Popper Content {{ item.label }}
-          </div>
-
-          <!-- <button slot="reference"> -->
-          <!-- Reference Element -->
-          <div variant="light" slot="reference">
-            <div class="control-item-options">
-              <span />
-              
-            </div>
-            <img :src=item.iconSrc :alt=item.label>
-        
-          </div>
-            
-          <!-- </button> -->
-        </popper>
-       
+        <SubmenuPopper :data="item"/>
       </li>
 
       <li class="control-item">
@@ -41,53 +11,80 @@
           <img :src="plusIcon" :alt="$t('Add')">
         </div>
       </li>
-
-
     </ul>
     
-    
-
   </div>
 
 </template>
 
 <script>
-import Popper from 'vue-popperjs';
-import 'vue-popperjs/dist/vue-popper.css';
+import SubmenuPopper from './SubmenuPopper/SubmenuPopper.vue';
 export default ({
   components: {
-    Popper,
+    SubmenuPopper,
   },
   data() {
     return {
       controlList:[
         {
-          iconSrc: '/img/start-event.e895cee2.svg',
+          iconSrc: require('@/assets/toolpanel/start-event.svg'),
           label: this.$t('Start Event'),
           id: 'startEvent',
+          items: [
+            {
+              iconSrc: require('@/assets/toolpanel/start-event.svg'),
+              label: this.$t('Start Event'),    
+              id: 'genericStartEvent',
+            },
+            {
+              iconSrc: require('@/assets/toolpanel/message-start-event.svg'),
+              label: this.$t('Message Start Event'),    
+              id: 'messageStartEvent',
+            },
+            {
+              iconSrc: require('@/assets/toolpanel/conditional-start-event.svg'),
+              label: this.$t('Conditional Start Event'),    
+              id: 'conditionalStartEvent',
+            },
+            {
+              iconSrc: require('@/assets/toolpanel/signal-start-event.svg'),
+              label: this.$t('Signal Start Event'),    
+              id: 'signalStartEvent',
+            },
+            {
+              iconSrc: require('@/assets/toolpanel/timer-start-event.svg'),
+              label: this.$t('Start Timer Event'),    
+              id: 'timerStartEvent',
+            },
+          ],
         },
         {
-          iconSrc: '/img/generic-intermediate-event.ba2e1d71.svg',
+          // iconSrc: '/img/generic-intermediate-event.ba2e1d71.svg',
+          iconSrc: require('@/assets/toolpanel/generic-intermediate-event.svg'),
           label: this.$t('Intermediate Event'),
           id: 'intermediateEvent',
         },
         {
-          iconSrc: '/img/end-event.1ad31396.svg',
+          // iconSrc: '/img/end-event.1ad31396.svg',
+          iconSrc: require('@/assets/toolpanel/end-event.svg'),
           label: this.$t('End Event'),
           id: 'endEvent',
         },
         {
-          iconSrc: '/img/task.e7419065.svg',
+          // iconSrc: '/img/task.e7419065.svg',
+          iconSrc: require('@/assets/toolpanel/task.svg'),
           label: this.$t('Task'),
           id: 'task',
         },
         {
-          iconSrc: '/img/generic-gateway.3a76fc75.svg',
+          //iconSrc: '/img/generic-gateway.3a76fc75.svg',
+          iconSrc: require('@/assets/toolpanel/generic-gateway.svg'),
           label: this.$t('Gateway'),
           id: 'gateway',
         },
         {
-          iconSrc: '/img/pool.ac62ddae.svg',
+          // iconSrc: '/img/pool.ac62ddae.svg',
+          iconSrc: require('@/assets/toolpanel/pool.svg'),
           label: this.$t('Pool'),
           id: 'pool',
         },
---
+++ src/components/railBottom/controls/SubmenuPopper/SubmenuPopper.vue
@@ -0,0 +1,102 @@
+<template>
+  <popper
+
+    trigger="clickToToggle"
+    :options="{
+      placement: 'top',
+      modifiers: { offset: { offset: '0,20px' } }
+    }"
+    :visible-arrow=false
+  >
+    <div v-if="data.items">
+      <ul class="control-submenu">
+        <li v-for="(item, key) in data.items" class="control-submenu-list"  :key="key">
+          <img :src=item.iconSrc :alt=item.label>
+          <div>
+            {{ item.label }}
+          </div>
+        </li>
+      </ul>
+    </div>
+
+    <!-- <button slot="reference"> -->
+    <!-- Reference Element -->
+    <div slot="reference">
+      <div class="control-item-options">
+        <span />
+        
+      </div>
+      <img :src=data.iconSrc :alt=data.label>
+  
+    </div>
+  </popper>
+</template>
+  
+<script>
+import Popper from 'vue-popperjs';
+import 'vue-popperjs/dist/vue-popper.css';
+export default ({
+  props: {
+    data: { type: Object },
+  },
+  components: {
+    Popper,
+  },
+  data() {
+    return {
+    };
+  },
+});
+
+</script>
+  
+<style lang="scss" scoped>
+.control-submenu {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 10px;
+  // gap: 10px;
+
+  // position: absolute;
+  width: 255px;
+  // height: 204px;
+  left: 616px;
+  background: #FFFFFF;
+  /* Shadow(SM) */
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.1);
+  // border-radius: 4px;
+  list-style: none;
+
+  &-list {
+    /* Auto layout */
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 12px 2px;
+    gap: 8px;
+
+    width: 235px;
+    height: 40px;
+
+    background: #FFFFFF;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 0;
+    align-self: stretch;
+    flex-grow: 0;
+    &:hover {
+      background: #EBEEF2;
+    }
+    & > img {
+      width: 24px;
+      height: 24px;
+    }
+  }
+  
+}
+</style>
+  
