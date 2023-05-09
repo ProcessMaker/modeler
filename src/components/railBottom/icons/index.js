e31fea650aaec865e9bea5f8efdcf9b6419a8a8c
Miguel Angel <miguelangeldev@icloud.com>
2023-05-09 13:08:48 -0400

    ref: update css and icon for mini paper button

--- src/components/railBottom/icons/index.js
+++ src/components/railBottom/icons/index.js
@@ -1,2 +1,3 @@
 export { default as MinusIcon } from './MinusIcon.vue';
 export { default as PlusIcon } from './PlusIcon.vue';
+export { default as MiniMapIcon } from './MiniMapIcon.vue';
--- src/components/railBottom/miniPaperControl/MiniPaperControl.vue
+++ src/components/railBottom/miniPaperControl/MiniPaperControl.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="mini-paper-box">
-    <button type="button" class="mini-paper-button">
-      <img :src="mapIcon" alt="Mini Paper" >
+    <button
+      type="button"
+      class="mini-paper-button"
+      :class="miniMapOpen ? 'mini-paper-button-active' : ''"
+      data-test="mini-map-btn"
+      @click="miniMapOpen = !miniMapOpen"
+      v-b-tooltip.hover
+      :title="miniMapOpen ? $t('Hide Mini-Map') : $t('Show Mini-Map')"
+    >
+      <MiniMapIcon />
     </button>
   </div>
 </template>
 
 <script>
+import { MiniMapIcon } from '@/components/railBottom/icons';
 
 export default ({
+  components: {
+    MiniMapIcon,
+  },
+  miniMapOpen(isOpen) {
+    this.$emit('toggle-mini-map-open', isOpen);
+  },
   data() {
     return {
-      mapIcon: require('@/assets/railBottom/mini-map.svg'),
+      miniMapOpen: false,
     };
   },
 });
