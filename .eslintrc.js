64a8b749d1c978bec225589f3dd3d2795d182209
Jose Chirivella <jose.chirivella@processmaker.com>
2023-07-21 13:11:54 -0500

    Upgraded eslint and jest * removed unneeded deps * Removed unneeded eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,27 +10,98 @@ module.exports = {
     'eslint:recommended',
   ],
 
+  plugins: ['jest', 'cypress'],
+
   rules: {
-    semi: ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    indent: ['error', 2, { SwitchCase: 1 }],
-    quotes: ['error', 'single'],
-    'object-curly-spacing': ['warn', 'always', { 'arraysInObjects': false }],
-    'no-debugger': 'off',
+    semi: [
+      'error',
+      'always',
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    quotes: [
+      'error',
+      'single',
+    ],
+    'object-curly-spacing': [
+      'warn',
+      'always',
+      {
+        arraysInObjects: false,
+      },
+    ],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'object-shorthand': 'error',
-    'space-before-function-paren': ['error', 'never'],
+    'space-before-function-paren': [
+      'error',
+      'never',
+    ],
     'keyword-spacing': 'error',
-    'vue/html-indent': ['error', 2, { alignAttributesVertically: false }],
-    'vue/html-closing-bracket-newline': ['error', {
-      singleline: 'never',
-      multiline: 'always',
-    }],
+    'vue/html-indent': [
+      'error',
+      2,
+      {
+        alignAttributesVertically: false,
+      },
+    ],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always',
+      },
+    ],
     'vue/html-self-closing': 'error',
     'vue/mustache-interpolation-spacing': 'error',
     'template-curly-spacing': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
   },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        'jest/globals': true,
+      },
+    },
+    {
+      files: [
+        '**/tests/e2e/**/*.cy.{j,t}s?(x)',
+      ],
+      rules: {
+        strict: 'off',
+        semi: ['error', 'always'],
+        'comma-dangle': ['error', 'always-multiline'],
+        indent: ['error', 2, { SwitchCase: 1 }],
+        'vue/html-indent': 'error',
+        quotes: ['error', 'single'],
+        'no-debugger': 'off',
+        'object-shorthand': 'error',
+        'space-before-function-paren': ['error', 'never'],
+        'keyword-spacing': 'error',
+        'prefer-arrow-callback': 'error',
+      },
+      env: {
+        mocha: true,
+        'cypress/globals': true,
+        node: true,
+      },
+    },
+  ],
 };
