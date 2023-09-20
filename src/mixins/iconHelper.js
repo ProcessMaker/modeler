0b4fe2836cf606a8a0556e4ad1442fabe48b439a
Sanja <sanja.cornelius@processmaker.com>
2023-09-20 13:54:39 -0700

    Create IconHelper

---
+++ src/mixins/iconHelper.js
@@ -0,0 +1,14 @@
+export default {
+    methods: {
+        containsSvg(icon) {
+            // Regular expression to match a URL pattern
+            const urlPattern = /^(https?:\/\/)?([\w-]+(\.[\w-]+)+)([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/;
+      
+            // Regular expression to check if the string contains '.svg'
+            const svgPattern = /\.svg/;
+      
+            // Check if the variable is a string and either a URL or contains '.svg'
+            return typeof icon === 'string' && (urlPattern.test(icon) || svgPattern.test(icon));
+        }
+    },
+}
