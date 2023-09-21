e5ea3637786730ccd772fb795acf1915ccc3b082
Rodrigo Quelca <rodrigo@processmaker.com>
2023-09-21 12:13:35 -0400

    disable multiplayer

--- src/components/modeler/Modeler.vue
+++ src/components/modeler/Modeler.vue
@@ -329,7 +329,7 @@ export default {
       showInspectorButton: true,
       inspectorButtonRight: 65,
       multiplayer: null,
-      isMultiplayer: true,
+      isMultiplayer: false,
     };
   },
   watch: {
