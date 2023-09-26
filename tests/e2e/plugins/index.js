17c5bdcc1200a99b69363bc2f9b64e46cebc5c96
Jose Chirivella <jose.chirivella@processmaker.com>
2023-09-26 12:34:23 -0500

    FOUR-8764 Disabling gpu when connecting to chrome

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -2,6 +2,12 @@ module.exports = (on, config) => {
   if (config.env.inProcessmaker) {
     config.baseUrl = 'https://processmaker.local.processmaker.com';
   }
+  on('before:browser:launch', (browser, launchOptions) => {
+    if (browser.name === 'chrome' && browser.isHeadless) {
+      launchOptions.args.push('--disable-gpu');
+      return launchOptions;
+    }
+  });
 
   require('@cypress/code-coverage/task')(on, config);
 
