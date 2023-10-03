eaffd95a9d13325f5902806ef668972a3b8416ec
Agustín Busso <90727999+agustinbusso@users.noreply.github.com>
2023-10-03 11:49:54 -0300

    Ui improvements

--- src/components/welcome/WelcomeMessage.vue
+++ src/components/welcome/WelcomeMessage.vue
@@ -1,45 +1,15 @@
 <template>
-  <div
-    v-if="!promptSessionId || promptSessionId === ''"
-    class="message d-flex flex-column w-100 align-items-center"
-  >
-    <div class="d-flex justify-content-center align-items-center flex-column justify-content-center">
-      <div class="w-100">
-        {{
-          $t("To start, click and drop objects to build the process you like.")
-        }}
-      </div>
-      <div class="justify-content-center align-items-center w-100 text-center my-4">
-        <div class="text-align-center d-flex text-center justify-content-center align-items-center">
-          <div class="hyphen" />
-          {{ $t("or") }}
-          <div class="hyphen" />
-        </div>
-      </div>
-    </div>
+  <div class="message d-flex flex-column w-100 align-items-center">
     <b-button
-      class="new-process-btn d-flex flex-row p-2 align-items-center"
-      @click="redirectToAiProcess()"
-    >
-      <inline-svg :src="proceC2Icon" class="mx-2 ai-icon" />
-      <div class="mr-3">{{ $t("Kick-start a new process with our generative AI") }}</div>
-      <span class="fa fa-chevron-right" />
-    </b-button>
-  </div>
-  <div
-    v-else
-    class="message d-flex flex-column w-100 align-items-center"
-  >
-    <b-button
-      class="new-process-btn d-flex flex-row px-2 py-4 align-items-center text-capitalize"
+      class="new-process-btn d-flex flex-row px-2 py-4 align-items-center"
       @click="redirectToAiProcess()"
     >
       <div class="d-flex justify-content-between">
         <inline-svg :src="proceC2Icon" class="mx-3 my-auto ai-icon" />
         <div class="mr-4 text-left">
-          <div class="h5 m-0">{{ $t("Continue your AI session") }}</div>
-          <div class="text-secondary font-weight-light">
-            {{ $t("Last session") + " " + session.time + " at " + session.date }}
+          <div class="h5 m-0">{{ aiProcessButtonTitle }}</div>
+          <div class="text-secondary font-weight-light small">
+            {{ aiProcessButtonSubtitle }}
           </div>
         </div>
       </div>
@@ -73,6 +43,7 @@ export default {
       session: {
         date: '12/12/12',
         time: '12:12:12',
+        username: 'admin',
       },
       processId: window.ProcessMaker?.modeler?.process?.id,
     };
@@ -91,6 +62,18 @@ export default {
       }
 
       return '';
+    },
+    aiProcessButtonTitle() {
+      if (!this.promptSessionId || this.promptSessionId === '') {
+        return this.$t('Create a process with AI');
+      }
+      return this.$t('Continue AI session');
+    },
+    aiProcessButtonSubtitle() {
+      if (!this.promptSessionId || this.promptSessionId === '') {
+        return this.$t('Kick-start an AI generated process');
+      }
+      return this.$t('Last session by:') + ' ' + this.session.username + ' | ' + this.session.date + ' | ' + this.session.time;
     },
   },
   mounted() {
@@ -142,6 +125,7 @@ export default {
   border-color: #0872c2;
   background-color: #ffffff;
   box-shadow: 0 3px 9px -3px #c2c2c2;
+  border-radius: 13px;
 }
 
 .new-process-btn:hover {
