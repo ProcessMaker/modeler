6b0fe6f257dfac6d015a2a5bde22387987a36d68
Teisha McRae <mcraeteisha@gmail.com>
2023-11-21 17:04:41 -0500

    Add functionality for pinning PM Blocks in Modeler

--- src/components/rails/explorer-rail/pmBlocksLoop/pmBlocksLoop.vue
+++ src/components/rails/explorer-rail/pmBlocksLoop/pmBlocksLoop.vue
@@ -1,11 +1,45 @@
 <script>
+import pinIcon from '@/assets/pin-angle.svg';
+import pinFillIcon from '@/assets/pin-angle-fill.svg';
 import nodeTypesStore from '@/nodeTypesStore';
 import clickAndDrop from '@/mixins/clickAndDrop';
+import iconHelper from '@/mixins/iconHelper';
 
 export default {
   name: 'PmBlocksLoop',
-  mixins: [clickAndDrop],
+  mixins: [clickAndDrop, iconHelper],
+  data() {
+    return {
+      pinIcon,
+      pinFillIcon,
+      showPin: false,
+    };
+  },
+  created() {
+    nodeTypesStore.dispatch('getUserPinnedObjects');
+  },
+  methods: {
+    nodeTypeAlreadyPinned(object, type) {
+      return !!this.pinnedObjects.find(obj => obj.type === type);
+    },
+    unPin(object) {
+      this.deselect();
+      return nodeTypesStore.dispatch('removeUserPinnedObject', object);
+    },
+    addPin(object) {
+      this.deselect();
+      return nodeTypesStore.dispatch('addUserPinnedObject', object);
+    },
+  },
   computed: {
+    pinnedObjects() {
+      return nodeTypesStore.getters.getPinnedNodeTypes;
+    },
+    unpinnedObjects() {
+      const objects = this.pmBlockNodeTypes;
+      console.log('objects', objects);
+      return objects.filter((obj) => !this.pinnedObjects.some(pinnedObj => pinnedObj.type === obj.type));
+    },
     pmBlockNodeTypes() {
       return nodeTypesStore.getters.getPmBlockNodeTypes;
     },
@@ -25,19 +59,91 @@ export default {
       <template v-for="object in filteredPmBlockNodes">
         <div
           class="node-types__item"
+          :data-test="object.type"
           :key="object.id"
+          @mouseover="showPin = true"
+          @mouseleave="showPin = false"
           @click.stop="onClickHandler($event, object)"
         >
           <div class="d-flex">
             <i v-if="!object.svgIcon" class="node-types__item__icon" :class="object.customIcon"/>
             <img v-else class="node-types__item__icon" :src="object.svgIcon" :alt="$t(object.label)">
             <label>{{ $t(object.label) }}</label>
+            <img
+              v-if="nodeTypeAlreadyPinned(object, object.type)"
+              :src="pinFillIcon"
+              class="pinIcon"
+              alt="Unpin Element"
+              @click="unPin(object)"
+            >
+            <img
+              v-else
+              :src="pinIcon"
+              class="pinIcon"
+              alt="Pin Element"
+              @click="addPin(object)"
+            >
           </div>
         </div>
       </template>
     </div>
     <template v-if="filteredPmBlockNodes.length === 0 && !searchTerm">
-      <div class="pmBlocksContainer p-2">
+      <div class="pinnedObjects" v-if="pinnedObjects.length > 0">
+        <p>{{ $t('Pinned PM Blocks') }}</p>
+        <template v-for="pinnedObject in pinnedObjects">
+          <div
+            class="node-types__item"
+            :data-test="pinnedObject.type"
+            :key="pinnedObject.id"
+            @mouseover="showPin = true"
+            @mouseleave="showPin = false"
+            @click.stop="onClickHandler($event, pinnedObject)"
+          >
+            <i v-if="!containsSvg(pinnedObject.icon)" :class="pinnedObject.customIcon" class="fa-lg"/>
+            <img v-else class="node-types__item__icon" :src="pinnedObject.svgIcon" :alt="$t(pinnedObject.label)">
+            <span>{{ $t(pinnedObject.label) }}</span>
+            <img
+              :src="pinFillIcon"
+              class="pinIcon"
+              alt="Pin/Unpin Element"
+              @click="unPin(pinnedObject)"
+            >
+          </div>
+        </template>
+      </div>
+      <div class="objectCategory">
+        <p>{{ $t('PM Block Category') }}</p>
+        <template v-for="nodeType in unpinnedObjects">
+          <div
+            class="node-types__item"
+            :data-test="nodeType.type"
+            :key="nodeType.id"
+            @mouseover="showPin = true"
+            @mouseleave="showPin = false"
+            @click.stop="onClickHandler($event, nodeType)"
+          >
+            <i v-if="!containsSvg(nodeType.icon)" :class="nodeType.customIcon" class="fa-lg"/>
+            <img v-else class="node-types__item__icon" :src="nodeType.svgIcon" :alt="$t(nodeType.label)">
+            <span>{{ $t(nodeType.label) }}</span>
+            <img
+              :src="pinIcon"
+              class="pinIcon"
+              alt="Pin/Unpin Element"
+              @click="addPin(nodeType)"
+            >
+          </div>
+        </template>
+      </div>
+    </template>
+
+
+
+
+
+
+
+
+      <!-- <div class="pmBlocksContainer p-2">
         <template v-for="nodeType in pmBlockNodeTypes">
           <div
             class="pm-block-node-types__item p-2 d-block"
@@ -51,12 +157,16 @@ export default {
             </div>
           </div>
         </template>
-      </div>
-    </template>
+      </div> -->
   </div>
 </template>
 
 <style lang="scss">
+#pmBlockNodeTypesList {
+  .pinnedObjects {
+    margin-bottom: 1rem;
+  }
+}
 .pm-block-node-types {
   &__item {
     display: block;
@@ -66,6 +176,9 @@ export default {
     margin-bottom: 8px;
     &:hover {
       background-color: #EBEEF2;
+      .pinIcon {
+        background-color: #DADDDF;
+      }
     }
     &__icon {
       width: 1.5rem;
@@ -80,6 +193,11 @@ export default {
       margin-left: 1.6rem;
       font-size: 13px;
       color:#6C757D;
+    }
+    .pinIcon {
+      margin-left: auto;
+      border-radius: 4px;
+      padding: 0.3rem;
     }
   }
 }
