7a8d0d9670a0272155223a0c38e85c2246feb9c9
danloa <danloa@gmail.com>
2024-07-17 11:50:04 -0400

    Adding documentation circle to intermediate events

--- src/components/nodes/intermediateEvent/shape.js
+++ src/components/nodes/intermediateEvent/shape.js
@@ -17,6 +17,13 @@ export default shapes.standard.Circle.extend({
     tagName: 'image',
     selector: 'image',
   },
+  {
+    tagName: 'circle',
+    selector: 'doccircle',
+  }, {
+    tagName: 'text',
+    selector: 'doclabel',
+  },
   markersMarkup('topLeft'),
   markersMarkup('topCenter'),
   markersMarkup('topRight'),
@@ -29,6 +36,17 @@ export default shapes.standard.Circle.extend({
     type: 'processmaker.components.nodes.intermediateEvent.Shape',
     size: { width: 36, height: 36 },
     attrs: {
+      'doclabel': {
+        'ref-x': 26,
+        'ref-y': -4, ref: 'circle', fontSize: 20, fontWeight: 'bold',
+        width: 16, height: 16, 'data-test': 'nodeDocLabel', 'text':'',
+        fill: 'white', display: 'none',
+      },
+      'doccircle': {
+        'cx': 30, 'cy': 5, 'r': 10,
+        'label': '7',
+        'fill': '#8DC8FF', 'stroke': '#2B9DFF', 'strokeWidth': '3', 'display': 'none',
+        ref: 'rect', width: 15, height: 15, 'data-test': 'nodeDocCircle' },
       'body': { fill: intermediateColor, stroke: intermediateColorStroke },
       'body2': { cx: 18, cy: 18, r: 15, 'stroke-width': 2, fill: intermediateColor, stroke: intermediateColorStroke },
       'image': { 'ref-x': 5, 'ref-y': 5, ref: 'circle', width: 26, height: 26 },
