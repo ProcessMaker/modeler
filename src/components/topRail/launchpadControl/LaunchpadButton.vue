67f634b8caf0446b9d4aeed584f0ca91f7649e85
Fabio <frguachallab@gmail.com>
2025-01-09 14:15:00 -0400

    FOUR-20596: Show the modal if the process was not publish

---
+++ src/components/topRail/launchpadControl/LaunchpadButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    type="button"
+    class="btn btn-white"
+    :title="$t('Open Launchpad')"
+    v-b-tooltip.hover.viewport.d50="{ customClass: 'no-pointer-events' }"
+    data-cy="launchpad-button"
+    @mouseleave="handleMouseLeave"
+    @mouseover="handleMouseOver" 
+    @click.prevent="handleOpenLaunchpad"
+  >
+    <i :class="iconOpen" />
+  </button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      iconOpen: 'fas fa-play',
+    };
+  },
+  methods: {
+    handleOpenLaunchpad() {
+      this.$emit('verifyLaunchpad', window.ProcessMaker.modeler.launchpad === null);
+    },
+    handleMouseOver() { 
+      this.iconOpen = 'fas fa-external-link-alt'; 
+    }, 
+    handleMouseLeave() {
+      this.iconOpen = 'fas fa-play';
+    },
+  },
+};
+</script>
