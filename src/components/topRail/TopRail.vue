8e7d435b419f283b4a65e162482ae465d00d038d
Henry Jonas <jonathan.quispe@processmaker.com>
2025-03-18 22:38:53 -0400

    FOUR-22485: Open launchpad buttons appears templates edit

--- src/components/topRail/TopRail.vue
+++ src/components/topRail/TopRail.vue
@@ -20,10 +20,6 @@
       :warnings="warnings"
     />
     <slot />
-
-    <LaunchpadButton @verifyLaunchpad="handleVerifyLaunchpad"/>
-    <LaunchpadModal :show="showLaunchpadModal" @closeModal="closeModal" />
-
   </div>
 </template>
 
@@ -32,7 +28,6 @@ import store from '@/store';
 import { ValidateButton, ValidateIssue, ValidatePanel } from '@/components/topRail/validateControl';
 import MultiplayerViewUsers from '@/components/topRail/multiplayerViewUsers/MultiplayerViewUsers';
 import AiGenerateButton from '../aiMessages/AiGenerateButton.vue';
-import { LaunchpadButton, LaunchpadModal } from './launchpadControl';
 
 export default {
   components: {
@@ -41,8 +36,6 @@ export default {
     ValidatePanel,
     AiGenerateButton,
     MultiplayerViewUsers,
-    LaunchpadButton,
-    LaunchpadModal,
   },
   props: {
     validationErrors: {
@@ -62,8 +55,6 @@ export default {
     return {
       isOpenIssue: false,
       isOpenPanel: false,
-      showLaunchpadModal: false,
-      openLaunchpad: false,
     };
   },
   computed: {
@@ -110,18 +101,6 @@ export default {
      */
     handleOpenPanel(value) {
       this.isOpenPanel = value;
-    },
-    handleVerifyLaunchpad(value) {
-      this.openLaunchpad = value;
-      if (this.openLaunchpad) {
-        this.showLaunchpadModal = true;
-      } else {
-        this.openLaunchpad = false;
-        window.location.href = '/process-browser/' + window.ProcessMaker.modeler.process.id;
-      }
-    },
-    closeModal() {
-      this.showLaunchpadModal = false;
     },
   },
 };
