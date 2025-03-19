f7791b6e9c20161f6b82a2c918b49b1ad60e511e
David Callizaya <davidcallizaya@gmail.com>
2025-03-19 17:45:42 -0400

    Add test for migrating interstitial tasks to element destination format inside pools

--- tests/e2e/specs/elementDestination/MigrateInterstitial2ElementDestination.cy.js
+++ tests/e2e/specs/elementDestination/MigrateInterstitial2ElementDestination.cy.js
@@ -20,4 +20,21 @@ describe('Test the migration of interstitial to element destination', () => {
 
   });
 
+  it('load a old process with interstitial enabled in pools and migrate it to the new element destination format', () => {
+    uploadXml('taskWithInterstitialInPools.xml');
+
+    assertDownloadedXmlContainsExpected(`
+      <bpmn:task id="node_2" name="Form Task 1" pm:allowInterstitial="true" pm:assignment="requester" pm:assignmentLock="false" pm:allowReassignment="false" pm:config="{&#34;web_entry&#34;:null}" pm:elementDestination="{&#34;type&#34;:&#34;displayNextAssignedTask&#34;}">
+    `);
+    assertDownloadedXmlContainsExpected(`
+      <bpmn:task id="node_12" name="Form Task 2" pm:allowInterstitial="true" pm:interstitialScreenRef="1" pm:assignment="requester" pm:assignmentLock="false" pm:allowReassignment="false" pm:config="{&#34;web_entry&#34;:null}" pm:elementDestination="{&#34;type&#34;:&#34;displayNextAssignedTask&#34;}">
+    `);
+    assertDownloadedXmlContainsExpected(`
+      <bpmn:task id="node_74" name="Form Task 3" pm:allowInterstitial="true" pm:assignment="requester" pm:assignmentLock="false" pm:allowReassignment="false" pm:config="{&#34;web_entry&#34;:null}" pm:elementDestination="{&#34;type&#34;:&#34;displayNextAssignedTask&#34;}">
+    `);
+    assertDownloadedXmlContainsExpected(`
+      <bpmn:task id="node_84" name="Form Task 4" pm:allowInterstitial="false" pm:assignment="requester" pm:assignmentLock="false" pm:allowReassignment="false" pm:config="{&#34;web_entry&#34;:null}" pm:elementDestination="{&#34;type&#34;:&#34;taskSource&#34;,&#34;value&#34;:null}">
+    `);
+  });
+
 });
