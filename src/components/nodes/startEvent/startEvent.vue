23ea3763c2f6694fbed6746e8b5983db46d69080
Rodrigo <rodrigoquelca@gmail.com>
2025-08-05 16:01:55 -0400

    fix lint errors

--- src/components/nodes/startEvent/startEvent.vue
+++ src/components/nodes/startEvent/startEvent.vue
@@ -1,7 +1,6 @@
 <script>
 import baseStartEvent from '@/components/nodes/baseStartEvent/baseStartEvent';
 import webEntryIcon from '!!svg-inline-loader!@/assets/webentry.svg';
-import emailStartIcon from '!!svg-inline-loader!@/assets/start-event-mail.svg';
 import coloredIcon from '@/components/iconColors';
 
 function isWebEntryEnabled(config) {
@@ -12,16 +11,6 @@ function isWebEntryEnabled(config) {
   const parsedConfig = JSON.parse(config);
 
   return parsedConfig && parsedConfig.web_entry && parsedConfig.web_entry.mode !== 'DISABLED';
-}
-
-function isEmailStartEnabled(config) {
-  if (!config) {
-    return false;
-  }
-
-  const parsedConfig = JSON.parse(config);
-
-  return parsedConfig && parsedConfig.email_start && parsedConfig.email_start?.enabled;
 }
 
 export default {
