f7a03627cc93e985e1e3899813b6e1b71ffabb22
sanja <52755494+sanjacornelius@users.noreply.github.com>
2026-01-21 13:12:49 -0800

    disable launchpad button when using a process template

--- src/components/topRail/launchpadControl/LaunchpadButton.vue
+++ src/components/topRail/launchpadControl/LaunchpadButton.vue
@@ -2,12 +2,14 @@
   <button
     type="button"
     class="btn btn-white"
+    :class="buttonDisabledClass"
     :title="$t('Open Launchpad')"
     v-b-tooltip.hover.viewport.d50="{ customClass: 'no-pointer-events' }"
     data-cy="launchpad-button"
     @mouseleave="handleMouseLeave"
     @mouseover="handleMouseOver" 
     @click.prevent="handleOpenLaunchpad"
+    :disabled="disabled"
   >
     <i :class="iconOpen" />
   </button>
@@ -15,21 +17,40 @@
 
 <script>
 export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
   data() {
     return {
       iconOpen: 'fas fa-play',
+      buttonDisabledClass: 'no-hover',
     };
   },
   methods: {
     handleOpenLaunchpad() {
-      this.$emit('verifyLaunchpad', window.ProcessMaker.modeler.launchpad === null);
+      if (!this.disabled) {
+        this.$emit('verifyLaunchpad', window.ProcessMaker.modeler.launchpad === null);
+      }
     },
     handleMouseOver() { 
-      this.iconOpen = 'fas fa-external-link-alt'; 
+      if (!this.disabled) {
+        this.iconOpen = 'fas fa-external-link-alt'; 
+      }
     }, 
     handleMouseLeave() {
-      this.iconOpen = 'fas fa-play';
+      if (!this.disabled) {
+        this.iconOpen = 'fas fa-play';
+      }
     },
   },
 };
 </script>
+
+<style scoped> 
+  .no-hover:hover {
+    background-color: transparent;
+  }
+</style>
