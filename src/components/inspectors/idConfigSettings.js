61bdc82d2515f5908f6b6372801f0b4b3a727ad5
Nolan Ehrstrom <github@nolanpro.com>
2026-08-31 14:30:55 -0700

    Test

--- src/components/inspectors/idConfigSettings.js
+++ src/components/inspectors/idConfigSettings.js
@@ -1,5 +1,5 @@
 const idConfigSettings = {
-  label: 'Node Identifier',
+  label: 'Node Identifier FOO',
   helper: 'Enter the id that is unique from all other elements in the diagram',
   name: 'id',
   validation: 'required|regex:/^[_A-Za-z][-._A-Za-z0-9]*$/',
