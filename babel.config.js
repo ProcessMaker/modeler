818a643761fe0b20a2e6c00ad8ae3577f06dd9f3
Marco A. Nina Mena <marco.antonio.nina@processmaker.com>
2026-09-11 11:59:08 -0400

    Enhance Babel configuration to conditionally include Istanbul plugin based on environment

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset',
-  ],
-  sourceType: 'unambiguous',
+module.exports = (api) => {
+  const isProd = api.env('production');
+  api.cache.using(() => isProd);
+
+  return {
+    presets: [
+      '@vue/cli-plugin-babel/preset',
+    ],
+    plugins: isProd ? [] : ['istanbul'],
+    sourceType: 'unambiguous',
+  };
 };
